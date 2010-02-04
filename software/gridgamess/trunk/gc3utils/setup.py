ac1f3f5036f123eb239b262201f066f947843fdd
mpackard <mpackard@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-02-04 14:51:22 +0000

    fixed requires

--- software/gridgamess/trunk/gc3utils/setup.py
+++ software/gridgamess/trunk/gc3utils/setup.py
@@ -9,6 +9,6 @@ setup(name='gc3utils',
       author_email='kbadmin@oci.uzh.ch',
       url='http://www.gc3.uzh.ch/index.html',
       packages=['gc3utils'],
-      requires=['ase>=1.0',elixir>=1.0],
+      requires=['ase(>=1.0)','elixir(>=1.0)'],
      )
 
