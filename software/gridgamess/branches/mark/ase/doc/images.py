384855c455467cc9e3018b2e0e1d252faa254ced
markmonroe <markmonroe@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-02-04 09:29:29 +0000

    removed branches

--- software/gridgamess/branches/mark/ase/doc/images.py
+++
@@ -1,5 +0,0 @@
-# creates: ag.png water_divide_surf.png
-from urllib import urlretrieve
-urlretrieve('http://wiki.fysik.dtu.dk/ase-files/ag.png', 'ase/ag.png')
-urlretrieve('http://wiki.fysik.dtu.dk/ase-files/water_divide_surf.png',
-            'ase/dft/water_divide_surf.png')
