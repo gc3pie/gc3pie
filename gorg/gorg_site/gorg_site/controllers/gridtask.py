8760f6eb8a7211383b5419bba33c31a5cb18bbe3
mmonroe <mmonroe@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-02-26 09:53:04 +0000

    ***

---
+++ gorg/gorg_site/gorg_site/controllers/gridtask.py
@@ -0,0 +1,30 @@
+import logging
+
+from pylons import request, response, session, tmpl_context as c
+from pylons.controllers.util import abort, redirect_to
+
+from gorg_site.lib.base import BaseController, render
+
+log = logging.getLogger(__name__)
+
+class GridtaskController(BaseController):
+
+    def index(self):
+        # Return a rendered template
+        #return render('/gridtask.mako')
+        # or, return a response
+        return 'Hello World'
+
+    def submit_form(self):
+        return render('/submit_task_form.mako')
+
+    def upload(self):
+        xmlController = XmlgridtaskController()        
+        myfile = request.POST['myfile']
+        title = request.POST['title']
+        author = request.POST['author']
+        type = 'GAMESS'
+        xmlController.upload(myfile,  title,  author,  type)
+        c.mess = 'Successfully uploaded: %s, title: %s' % \
+                (myfile, title)
+        return render('/submit_task_finish.mako')
