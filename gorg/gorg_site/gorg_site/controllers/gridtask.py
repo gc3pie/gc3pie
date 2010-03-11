003ba124b5830b4c5e2ff88a51d815db850b9b7d
mmonroe <mmonroe@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-03-11 13:16:01 +0000

    ***

--- gorg/gorg_site/gorg_site/controllers/gridtask.py
+++ gorg/gorg_site/gorg_site/controllers/gridtask.py
@@ -4,7 +4,10 @@ from pylons import request, response, session, tmpl_context as c
 from pylons.controllers.util import abort, redirect_to
 
 from gorg_site.lib.base import BaseController, render
-from gorg_site.model.gridjob import GridtaskModel
+from gorg_site.model.gridjob import GridjobModel
+from gorg_site.model.gridtask import GridtaskModel
+
+from gorg_site.lib.mydb import Mydb
 
 log = logging.getLogger(__name__)
 
@@ -16,8 +19,15 @@ class GridtaskController(BaseController):
         # or, return a response
         return 'Hello World'
 
-    def submit_form(self):
-        return render('/submit_task_form.mako')
+    def query_form(self):
+        return render('/query_task_form.mako')
+    
+    def query(self):
+        db=Mydb().cdb()
+        task_id=request.POST['task_id']
+        c.task = GridtaskModel().load(db, task_id)
+        c.job_list = c.task.get_jobs(db)
+        return render('/query_task_finish.mako')
 
     def create(self):
         new_task = GridtaskModel()
