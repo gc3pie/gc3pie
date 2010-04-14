0e12fd223dc111e29dec09a1800b7352b39104f1
mpackard <mpackard@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-04-14 12:02:32 +0000

    lots of changes

--- gorg/gorg/utils.py
+++ gorg/gorg/utils.py
@@ -12,6 +12,8 @@ import logging
 import logging.handlers
 import ConfigParser
 from optparse import OptionParser
+from ArcLRMS import *
+from SshLRMS import *
 #from ase-patched import *
 
 homedir = os.path.expandvars('$HOME')
@@ -22,69 +24,63 @@ default_joblist_lock = rcdir + "/.joblist_lock"
 default_job_folder_location="$PWD"
 default_wait_time = 3
 
+'''
 
- 
-def ParseOptions(self,program_name,args):
-    """
-    Parse our general command line options.
+class Hcli:
 
-     * Takes as input a program name and the command line arguments.
-     * Returns: dictionary of options (options), list of remaining arguments (args)
- 
-    """
+    resource_list = {}
+    defaults = {}
+
+
+    def __init__(self, config_file_location):
+        try:
+            # probably don't need this stuff
+            # read configuration file
+            """
+            _local_resource_list = {}
+            (self.defaults,_local_resource_list) = readConfig(config_file_location)
+
+
+            for _resource in _local_resource_list.values():
+                if ("ncores" in _resource) & ("memory_per_core" in _resource) & \
+                        ("walltime" in _resource) & ("type" in _resource) & ("frontend" in _resource) \
+                        & ("applications" in _resource):
+                    # Adding valid resources
+                    logging.debug('Adding valid resource description [ %s ]',_resource['resource_name'])
+                    self.resource_list[_resource['resource_name']] = _resource
+
+            # Check if any resource configuration has been leaded
+            if ( len(self.resource_list) == 0 ):
+                raise Exception('could not read any valid resource configuration from config file')
+
+            logging.info('Loading configuration file %s \t[ ok ]',config_file_location)
+            """
+        except:
+            """
+            logging.critical('Failed init gcli')
+            """
+            raise
+'''
+   
+def Task(self,username,tasktitle,tasktype):
+    """Create a task.""" 
     
-    _usage = "Usage: %prog [options] jobid"
-    
-    parser = OptionParser(usage=_usage)
-    
-    # todo : remove dummy options
-
-    # common options 
-    parser.add_option("-t","--task", action="store", dest="tasktype", default=None, help="Which type of task to perform.")
-    parser.add_option("-v", action="count", dest="verbosity", default=0, help="Set verbosity level")
-    parser.add_option("-e","--example", action="store", dest="example", default=None, help="example")
-    parser.add_option("-w","--woo", action="store", dest="woo", default=None, help="dummy")
-    parser.add_option("-d", "--dir", dest="directory",default='~/tasks', help="Directory to save tasks in.")
-    parser.add_option("-f", "--file", dest="file",default='exam01.inp', help="Gamess inp to restart from.")
-    parser.add_option("-n", "--db_name", dest="db_name", default='gorg_site', help="Database name.")
-    parser.add_option("-l", "--db_loc", dest="db_loc", default='http://127.0.0.1:5984', help="Database URI.")
-
-    # handle tasktype-specific options
+    # Handle tasktype-specific things.
     if tasktype == 'hessian':
-        parser.add_option("-x","--xoo", action="store", dest="xoo", default=None, help="example")
-    elif tasktype == 'single':
-        parser.add_option("-y","--yoo", action="store", dest="yoo", default=None, help="example")
-    elif tasktype == 'restart':
-        parser.add_option("-z","--zoo", action="store", dest="zoo", default=None, help="example")
-  
-    (options, args) = parser.parse_args()
-
-    return options, args
-
-
-def SetupDatabase():
-    """Initialize the database.  
-     * If the database does not exist, create it.
-     * If the tables do not exist, create them.
-     
-     Return a database object.
-    """ 
-    # todo : do we return a database object, or a database connection object, or ???
+        print 'hessian'
+    elif tasktype == 'singlejob':
+        print 'singlejob'
+    elif tasktype == 'continue':
+        print 'continue'
     
-    dbhost = 'http://127.0.0.1'
-    port = '5984'
-    address = dbhost + ":" + port
-    database = Mydb('gorg_site',address).createdatabase()
-    database = Mydb('gorg_site',address).cdb()
-    
-    # todo : find out what this does
-    GridjobModel.sync_views(database)
-    GridrunModel.sync_views(database)
-    GridtaskModel.sync_views(database)
-    BaseroleModel.sync_views(database)
-    
-    return database
+    try:
+        task = GridtaskModel().create(username, tasktitle)
+    except:
+        raise
 
+    return task
+
+ 
 def CheckInputFile(inputfile):
     """Perform various checks on the input file."""
     
@@ -97,3 +93,77 @@ def CheckInputFile(inputfile):
         return False
         
 
+      
+def main():
+    global default_job_folder_location
+    global default_joblist_location
+    global default_joblist_lock
+
+    try:
+ 
+        
+        program_name = sys.argv[0]
+        # todo : do we need this?  does the parser automatically know to look at the arguments?
+        sysargs = sys.argv
+        
+        options, args = ParseOptions(program_name, sysargs)
+
+        # Configure logging service
+        logger = CreateBasicLogger(options.verbosity)
+        
+        if options.input == None:
+            logging.info('No input file specified.  Exiting.')
+            sys.exit(1)
+                    
+ 
+        # Check input file.  Can be made fancy later.
+        try:
+            CheckInputFile(options.input)
+        except:
+            raise
+        
+        #db=Mydb(options.db_name,options.db_loc).cdb()
+        database = SetupDatabase()
+        
+        # Call a different method depending on the type of task we want to create.
+        if options.tasktype == 'hessian':
+            try:
+                # task = HessTask()
+                gamess_calc = GamessGridCalc(database)
+                ghess = GHessian(database, gamess_calc, logging_level)
+                ghess.run(atoms, params)
+            except:
+                raise     
+        elif options.tasktype == 'single':
+            try:
+                #task = SinglejobTask()
+                gamess_calc = GamessGridCalc(database)
+                gsingle = GSingle(database, gamess_calc, logging_level)
+                gsingle.run(atoms, params)                
+            except:
+                raise           
+        elif options.tasktype == 'restart':
+            try:
+                #task = ContinueTask()
+                gamess_calc = GamessGridCalc(database)
+                grestart = GRestart(db, gamess_calc, logging_level)
+                grestart.run(atoms, params)
+            except:
+                raise
+        else:
+            logging.error('unknown task type')
+            sys.exit(1)     
+
+        #hcli = Hcli(default_config_file_location)
+        
+        logger.error("your task id is" + task.id)
+        
+
+    except:
+        raise
+        sys.exit(1)
+       
+                
+if __name__ == "__main__":
+      sys.exit(main())
+      
