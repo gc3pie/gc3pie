a2f34377786ef8e4808415706557e12f01e25ea3
mmonroe <mmonroe@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-04-23 10:54:22 +0000

    ***

---
+++ branches/mike/markstools/markstools/lib/exceptions.py
@@ -0,0 +1,7 @@
+_log = logging.getLogger('markstools')
+
+class MarksToolsException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
