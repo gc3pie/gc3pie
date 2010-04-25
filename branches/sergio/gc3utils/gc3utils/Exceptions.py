b805090218d75b54a6baf2bb29db3fb9e8f13911
sergio <sergio@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-04-25 23:44:24 +0000

    Sergio: added mising module Exceptions

---
+++ branches/sergio/gc3utils/gc3utils/Exceptions.py
@@ -0,0 +1,24 @@
+class BrokerException(Exception):
+    #    def __init__(self,msg):
+    pass
+#        logging.error(msg)
+
+class LRMSException(Exception):
+    #    def __init__(self,msg):
+    pass    
+#    def __init__(self,msg):
+#        logging.error(msg)
+
+class TransportException(Exception):
+    #    def __init__(self,msg):
+    pass
+#    def __init__(self,msg):
+#        logging.error(msg)
+
+class AuthenticationException(Exception):
+    #    def __init__(self,msg):
+    pass
+#    def __init__(self,msg):
+#        logging.error(msg)
+
+    
