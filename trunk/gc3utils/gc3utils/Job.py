9c9c9672a11ed10484b533f53357181c8f620a41
sergio <sergio@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-05-05 13:05:33 +0000

    erroneous placement of constant values

--- trunk/gc3utils/gc3utils/Job.py
+++ trunk/gc3utils/gc3utils/Job.py
@@ -4,14 +4,14 @@ from InformationContainer import *
 # -----------------------------------------------------
 # Job
 #
+JOB_STATE_FINISHED = 1
+JOB_STATE_RUNNING = 2
+JOB_STATE_FAILED = 3
+JOB_STATE_SUBMITTED = 4
+JOB_STATE_COMPLETED = 5
+
 
 class Job(InformationContainer):
-
-    JOB_STATE_FINISHED = 1
-    JOB_STATE_RUNNING = 2
-    JOB_STATE_FAILED = 3
-    JOB_STATE_SUBMITTED = 4
-    JOB_STATE_COMPLETED = 5
 
     def is_valid(self):
         if self.__dict__.has_key('status') and self.__dict__.has_key('resource_name') and self.__dict__.has_key('lrms_jobid'):
