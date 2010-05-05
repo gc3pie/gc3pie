d84790209a1fcf2f0d354c8f215a242f375260c2
mpackard <mpackard@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-05-05 08:37:11 +0000

    removing mike branch junk

--- branches/mike/markstools/markstools/lib/exceptions.py
+++
@@ -1,7 +0,0 @@
-import markstools
-
-class UnhandledStateError(Exception):
-    def __init__(msg, report=True):
-       super(Exception).__init__(msg)
-       if report:
-           log.critical(msg)
