99f1b3e914b1bae0cf68a9cc617cfef035e83d39
murri <murri@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-05-11 16:48:43 +0000

    Ticket #11: use agreed logging style in `utils.py` and `Authorization.py`.
    modified:
      gc3utils/Authorization.py
      gc3utils/utils.py

--- gc3utils/gc3utils/Authorization.py
+++ gc3utils/gc3utils/Authorization.py
@@ -1,15 +1,16 @@
 import Default
-import logging
 import utils
 import sys
 import os
 import Exceptions
 
+import gc3utils
+
+
 class Auth(object):
     def __init__(self,auto_enable=True):
         self.auto_enable=True
         self.__auths = { }
-        self.log = logging.getLogger('gc3utils')
 
     def get(self,auth_type):
         if not self.__auths.has_key(auth_type):
@@ -38,16 +39,14 @@ class Auth(object):
         return a
 
 class ArcAuth(object):
-    def __init__(self):
-        self.log = logging.getLogger('gc3utils')
                 
     def check(self):
-        self.log.debug('Checking authentication: GRID')
+        gc3utils.log.debug('Checking authentication: GRID')
         try:
             if (utils.check_grid_authentication()) and (utils.check_user_certificate()):
                 return True
         except:
-            self.log.error('Authentication Error: %s', sys.exc_info()[1])
+            gc3utils.log.error('Authentication Error: %s', sys.exc_info()[1])
         
         return False
      
@@ -57,21 +56,21 @@ class ArcAuth(object):
             _aaiUserName = None
                 
             Default.AAI_CREDENTIAL_REPO = os.path.expandvars(Default.AAI_CREDENTIAL_REPO)
-            self.log.debug('checking AAI credential file [ %s ]',Default.AAI_CREDENTIAL_REPO)
+            gc3utils.log.debug('checking AAI credential file [ %s ]',Default.AAI_CREDENTIAL_REPO)
             try: 
                 _fileHandle = open(Default.AAI_CREDENTIAL_REPO,'r')
                 _aaiUserName = _fileHandle.read()
                 _fileHandle.close()
                 _aaiUserName = _aaiUserName.rstrip("\n")
-                self.log.debug('_aaiUserName: %s',_aaiUserName)
+                gc3utils.log.debug('_aaiUserName: %s',_aaiUserName)
             except IOError:
-                self.log.error('Failed opening AAI credential file') 
+                gc3utils.log.error('Failed opening AAI credential file') 
 
             # Renew credential
             return utils.renew_grid_credential(_aaiUserName)
 
         except:
-            self.log.critical('Failed renewing grid credential [%s]',sys.exc_info()[1])
+            gc3utils.log.critical('Failed renewing grid credential [%s]',sys.exc_info()[1])
             # return False
             raise Exceptions.AuthenticationException('failed renewing GRID credential')
 
