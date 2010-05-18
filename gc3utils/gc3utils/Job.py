6e1656a0a8197824bb5d4d9f3665beb07da61d90
mpackard <mpackard@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-05-18 15:29:51 +0000

    Added comments describing what the various states mean.

--- gc3utils/gc3utils/Job.py
+++ gc3utils/gc3utils/Job.py
@@ -4,11 +4,34 @@ from InformationContainer import *
 # -----------------------------------------------------
 # Job
 #
+
+# Job is finished on grid or cluster, results have not yet been retrieved.
+# User or Application can now call gget.
 JOB_STATE_FINISHED = 1
+
+# Job is currently running on a grid or cluster.
+# Gc3utils must wait until it finishes to proceed.
 JOB_STATE_RUNNING = 2
+
+# Could mean several things:
+#   - LRMS failed to accept the job for some reason.
+#   - LRMS killed the job.
+#   - Job exited with non-zero exit status.
+# The User can decide whether to do gget or resubmit or nothing.
 JOB_STATE_FAILED = 3
+
+# Job is between the submit phase and confirmed in the LRMS scheduler.
+# For example, this applies to ARC jobs after they are submitted but not yet officially queued according to the ARC scheduler.
+# SGE+SSH jobs do not have this.
+# No action required.
 JOB_STATE_SUBMITTED = 4
+
+# Job is finished and results successfully retrieved.
+# User or Application can do whatever it wants with the results.
 JOB_STATE_COMPLETED = 5
+
+# This is the default initial state of a job instance before it has been updated by a gsub/gstat/gget/etc.
+# No action required; the state will be set to something else as the code executes.
 JOB_STATE_UNKNOWN = 6
 
 
