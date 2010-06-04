389d3813fcf915bb98e9b7f65f8f819f98f5a453
mmonroe <mmonroe@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-06-04 09:21:33 +0000

    Tagging the .1 functioning release

--- websites/gorg-site/gorg-site/tests/functional/test_xmlgridjob.py
+++
@@ -1,7 +0,0 @@
-from gorg_site.tests import *
-
-class TestXmlgridjobController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='xmlgridjob', action='index'))
-        # Test response...
