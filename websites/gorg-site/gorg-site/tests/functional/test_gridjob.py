716be9ed34ad16698ddaa1d719f0e2d104c11b25
mmonroe <mmonroe@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-06-04 09:22:57 +0000

    Tagging the .1 functioning release

--- websites/gorg-site/gorg-site/tests/functional/test_gridjob.py
+++
@@ -1,7 +0,0 @@
-from gorg_site.tests import *
-
-class TestGridjobController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='gridjob', action='index'))
-        # Test response...
