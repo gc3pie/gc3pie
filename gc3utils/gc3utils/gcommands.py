be8caa3070806efcf0c0331797b7d279d2d9e997
murri <murri@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-06-18 17:28:41 +0000

    Make "-v" option fully functional again. (Fixes ticket #77)
    modified:
      gc3utils/etc/logging.conf.example
      gc3utils/gcmd.py
      gc3utils/gcommands.py

--- gc3utils/gc3utils/gcommands.py
+++ gc3utils/gc3utils/gcommands.py
@@ -42,7 +42,7 @@ def _configure_logger(verbosity):
     """
     Configure the logger verbosity.
     """
-    logging_level = 10 * min(1, 6-verbosity)
+    logging_level = 10 * max(0, 5-verbosity)
     gc3utils.log.setLevel(logging_level)
 
 
