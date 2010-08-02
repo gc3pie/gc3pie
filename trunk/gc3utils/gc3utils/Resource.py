494751ee997fb993ee124effadd86f89f7aaa6ed
riccardo.murri <riccardo.murri@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-08-02 14:41:17 +0000

    Reworked sample configuration file. Documented the values actually used in `Resource` objects.
    modified:
      gc3utils/Resource.py
      gc3utils/etc/gc3utils.conf.example

--- trunk/gc3utils/gc3utils/Resource.py
+++ trunk/gc3utils/gc3utils/Resource.py
@@ -1,38 +1,51 @@
-import types
-from InformationContainer import *
+from InformationContainer import InformationContainer
+#import sys
+#from utils import Struct
 
 # -----------------------------------------------------
 # Resource
 #
-
 class Resource(InformationContainer):
     '''
-    int max_cores_per_node
-    int max_walltime
-    int max_memory_per_node
-    int queued
-    int running
-    int max_user_queue
-    int max_user_run
-    int user_queued
-    int user_run
-    string frontend
-    string name
-    int total_cores
-    int max_quota
-    int used_quota
-    int type
+    `Resource` objects are dictionaries, comprised of the following keys.
+    
+    Statically provided, i.e., specified at construction time and changed never after:
+
+      arc_ldap             string   
+      authorization_type   string
+      frontend             string
+      gamess_location      string
+      max_cores_per_job    int     *
+      max_memory_per_core  int     *
+      max_walltime         int     *
+      name                 string  *
+      ncores               int
+      type                 int     *
+      walltime             int(?)
+
+    Starred attributes are required for object construction.
+     
+    Dynamically provided (i.e., defined by the `get_resource_status()` method or similar):
+      free_slots          int
+      user_run            int
+      user_queued         int
+      queued              int
     '''
 
     def is_valid(self):
-        if (self.has_key('max_cores_per_node') 
+        if (self.has_key('max_cores_per_job') 
+            and self.has_key('max_memory_per_core') 
             and self.has_key('type') 
-            #and self.has_key('frontend') 
             and self.has_key('name') 
             and self.has_key('max_walltime') 
-            and self.has_key('max_memory_per_node') 
-            and self.has_key('total_cores')
             ):
             return True
         else:
             return False
+
+    # def __getattr__(self, key):
+    #     sys.stderr.write("Resource: query for attribute '%s'\n" %key)
+    #     return Struct.__getattr__(self, key)
+    # def __getitem__(self, key):
+    #     sys.stderr.write("Resource: query for attribute '%s'\n" % key)
+    #     return dict.__getitem__(self, key)
