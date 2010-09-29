696129d646378c0611d9694e069dab3d3d2ce0d6
markjmonroe <markjmonroe@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-09-29 06:54:40 +0000

    First working copy of htpie that uses mongokit.

--- websites/gorg-site/debug_script.py
+++
@@ -1,2 +0,0 @@
-from paste.script.serve import ServeCommand
-ServeCommand("serve").run(["development.ini"])
--- websites/gorg-site/gorg-site/tests/functional/test_xmlgridtask.py
+++
@@ -1,7 +0,0 @@
-from gorg_site.tests import *
-
-class TestXmlgridtaskController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='xmlgridtask', action='index'))
-        # Test response...
