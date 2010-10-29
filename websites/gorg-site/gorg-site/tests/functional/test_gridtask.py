9df9c32a9c4fd16c2d6af7a7e1629e2b42ba8a81
markjmonroe <markjmonroe@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-10-29 09:37:37 +0000

    We are not using these anymore.

--- websites/gorg-site/gorg-site/tests/functional/test_gridtask.py
+++
@@ -1,7 +0,0 @@
-from gorg_site.tests import *
-
-class TestGridtaskController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='gridtask', action='index'))
-        # Test response...
