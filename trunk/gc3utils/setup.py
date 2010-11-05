282d726d2656d07214633b9260c6886bc4a3d554
riccardo.murri <riccardo.murri@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-11-05 09:53:35 +0000

    trunk/setup.py: Bump package version to 0.10.18

--- trunk/gc3utils/setup.py
+++ trunk/gc3utils/setup.py
@@ -33,7 +33,7 @@ import setuptools
 import setuptools.dist
 setuptools.setup(
     name = "gc3utils",
-    version = "0.7.21", # format: 0.MONTH.DAY (for now ...)
+    version = "0.10.18", # format: 0.MONTH.DAY (for now ...)
 
     packages = setuptools.find_packages(exclude=['ez_setup']),
     #scripts = ['gcmd.py'],
