397f6119e0b76847d288fb1114e5a712ef44ab9d
riccardo.murri <riccardo.murri@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2010-11-22 18:20:11 +0000

    Relocated HTML documentation into `docs/html`.
    added:
      docs/apidoc/html@
    renamed:
      docs/apidoc/_static/ => docs/_static/
      docs/apidoc/_templates/ => docs/_templates/
      docs/apidoc/html/ => docs/html/
    modified:
      docs/Makefile
      docs/conf.py
      docs/html/.buildinfo
      docs/html/objects.inv
      docs/html/searchindex.js

--- trunk/gc3utils/docs/conf.py
+++ trunk/gc3utils/docs/conf.py
@@ -64,7 +64,7 @@ release = '0.11.10'
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
-exclude_trees = ['apidoc']
+exclude_trees = ['html']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
@@ -120,7 +120,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['apidoc/_static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -164,10 +164,10 @@ htmlhelp_basename = 'gc3piedoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 # The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
+latex_paper_size = 'a4'
 
 # The font size ('10pt', '11pt' or '12pt').
-#latex_font_size = '10pt'
+latex_font_size = '10pt'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
