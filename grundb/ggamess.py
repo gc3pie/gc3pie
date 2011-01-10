02c6d315b964a0e0752cb5a494e322b8dfc5c1e6
riccardo.murri <riccardo.murri@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2011-01-10 18:12:14 +0000

    Ditto for ggamess.

--- grundb/ggamess.py
+++ grundb/ggamess.py
@@ -65,8 +65,9 @@ import time
 
 import gc3libs
 from gc3libs.application.gamess import GamessApplication
-import gc3libs.Default
 import gc3libs.core
+import gc3libs.Default
+import gc3libs.persistence
 import gc3libs.utils
 
 
@@ -232,15 +233,7 @@ def save(tasks, session, store):
 
 # create a `Persistence` instance to save/load jobs
 
-class _JobIdFactory(gc3libs.persistence.Id):
-    """
-    Override :py:class:`Id` behavior and generate IDs starting with a
-    lowercase ``job`` prefix.
-    """
-    def __new__(cls, obj, prefix=None, seqno=None):
-        return gc3libs.persistence.Id.__new__(cls, obj, 'job', seqno)
-
-store = gc3libs.persistence.FilesystemStore(idfactory=_JobIdFactory)
+store = gc3libs.persistence.FilesystemStore(idfactory=gc3libs.persistence.JobIdFactory)
 
 # load the session file, or create a new empty one if not existing
 try:
