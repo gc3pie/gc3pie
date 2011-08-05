8602f4549bf2472c993deae1fad6265f9000555b
timm.reumann@uzh.ch <timm.reumann@uzh.ch@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2011-08-05 15:59:15 +0000

    Removed csv-stuff, now using ":" and "," as delim.s, added "done"-flag, problem with list comprehension
    M    sepjobs.py

--- trunk/gc3pie/gc3apps/gamess/sepjobs.py
+++ trunk/gc3pie/gc3apps/gamess/sepjobs.py
@@ -23,6 +23,11 @@ Classify GAMESS output files according to keyword matches.
 __version__ = 'development version (SVN $Revision$)'
 # summary of user-visible changes
 __changelog__ = """
+  2011-08-04:
+    * Removed csv-stuff, cannot deal with ""I/O", ERROR"
+    * Now using ":" to separate searchstring from rest and "," for residual fields
+    * Found problem with list comprehension and os.walk in "search_for_input_directories"
+    * Added "done"-flag to make script ingoring already processed lines in config. file
   2011-08-03:
     * Use `.csv` file for configuration.
   2011-07-27:
@@ -35,7 +40,6 @@ __docformat__ = 'reStructuredText'
 
 # TAKES 106 MIN. FOR SEPARATING ~2100 OUTPUT FILES
 
-import csv
 import os
 import re
 import sys
@@ -47,23 +51,52 @@ import shutil
 import cli.log
 #import pstats
 
+
+def CLEANLIST(itmlst, delstr) :
+	delpos = []; p = 0					# list of deleting position, position
+	for itm in itmlst :
+		if re.match(delstr, itm) :		# "\" to suppress interpret. of "." by python
+			delpos.append(p)
+		p += 1
+	ndp = 0								# no. of deleted positions
+	for p in delpos :			
+		p -= ndp				# convert. pos. in old dirlist to pos. in current one
+		itmlst.pop(p)
+#		del dirlist(p)			# gives error "can't delete function call"
+		ndp += 1
+	return itmlst
+
+
 def search_for_input_directories(root, names):
     """
     Return list of directories under `root` (at any nesting level)
     whose name is contained in the list `names`.
     """
-    result = [ ]
+    result = []
     for rootpath, dirs, files in os.walk(root):
-        found_dirname = [ ]
+        matching_dirs = []
         # removing hidden directories from directory list
+        if os.path.basename(rootpath).startswith(r'.'):
+            print "DEBUG: current path:\n", rootpath
+        if rootpath == root: 
+            print "DEBUG: rootpath:\n", rootpath
+            print "DEBUG: raw dirs:\n", dirs
+        """
         dirs = [ d for d in dirs if not d.startswith(r'.') ]
+        This produces the correct output according to print, but os.walk
+        somehow ignores it - still goes into .dirname
+        """
+        dirs = CLEANLIST(dirs, '\.')
+        if rootpath == root: 
+            print "DEBUG: cleaned dirs:\n", dirs
         for dirname in dirs:
             if dirname in names:
                 result.append(os.path.join(rootpath, dirname))
-                found_dirname.append(dirname)
+                matching_dirs.append(dirname)
         # removing assigned directories from directory list
-        for dirname in found_dirname:
-            dirs.remove(dirname)        
+        for dirname in matching_dirs:
+            dirs.remove(dirname)
+    print "DEBUG: matching dirs:\n", result    
     return result
 
 
@@ -75,7 +108,8 @@ def search_for_input_files(root, ext):
     result = [ ]
     for rootpath, dirs, files in os.walk(root):
         # removing hidden directories from directory list
-        dirs = [ d for d in dirs if not d.startswith('.') ]
+#        dirs = [ d for d in dirs if not d.startswith('.') ]  see above
+        dirs = CLEANLIST(dirs, '\.')
         for filename in files:
             if filename.endswith(ext):
                 result.append(os.path.join(rootpath, filename))
@@ -98,35 +132,45 @@ def parse_kwfile(filename):
     Read `filename` and return a dictionary mapping a folder name into
     a corresponding regular expression to search for.
     """
-    csvfile = open(filename, 'r')
-    sample = csvfile.read(1024)
-    csvfile.seek(0)
-    dialect = csv.Sniffer().sniff(sample)
-    has_header = csv.Sniffer().has_header(sample)
-    reader = csv.reader(csvfile, dialect)
-    result = { }
-    for rownum, row in enumerate(reader):
-        if has_header and rownum == 0:
-            continue # skip header
-        if len(row) == 0:
-            continue # skip empty lines
-        foldername, searchtype, what_to_search = row[0:3]
-        #print "DEBUG: read row %r"% row
-        if len(row) >= 4:
-            start = float(row[3]) / 100.0
-        else:
-            start = 0.0
-        if len(row) >= 5:
-            end = float(row[4]) / 100.0
-        else:
-            end = 1.0
+    number = re.compile(r'[0-9]+\.?[0-9]*')
+    done = re.compile(r'done[ ,]*')
+    file = open(filename, 'rw')
+    result = { } ; newlns = []
+    for line in file:
+       
+    	newlns.append(line)
+        if line == '\n' or line.startswith('#') or line.isspace():
+            print "DEBUG: ignoring line %r"% line
+            continue        			# skip empty or comment lines
+        if done.search(line.rsplit(':', 1)[1]):   #line.endswith('done'):
+            print "DEBUG: ignoring line %r"% line
+            continue            # skip lines already used for separation
+            
+        print "DEBUG: processing line %r"% line
+        newlns[len(newlns)-1] = newlns[len(newlns)-1].rstrip(' \n') + ', done\n'
+        print "DEBUG: new lines %r"% newlns[len(newlns)-1]
+        what_to_search, rest = line.rsplit(':', 1)
+        print "DEBUG: search string:", str(what_to_search)
+        rest = rest.replace(' ', '').rstrip('\n') + ','
+        searchtype, foldername, rest = rest.split(',', 2)
+        print "DEBUG: searchtype, foldername:", searchtype, foldername
+        start = 0.0 ; end = 1.0
+        if number.match(rest):
+        	startstr, rest = rest.split(',', 1)
+        	start = float(startstr) / 100.0
+        if number.match(rest):
+        	endstr, rest = rest.split(',', 1)
+        	end = float(endstr) / 100.0
+        print "DEBUG: start, end:", start, end
         if searchtype in ('regexp', 're'):
             result[foldername] = (re.compile(what_to_search), start, end)
         else:
             # literal string search
             result[foldername] = (StringAdaptor(what_to_search), start, end)
-    #print "DEBUG: parse_kwfile result is '%s'" % result
-    csvfile.close()
+        
+#    print "DEBUG: parse_kwfile result is '%s'" % result
+#    file.writelines(newlns)
+    file.close()
     return result
 
 
@@ -163,7 +207,7 @@ def sepjobs(cmdline):
     unknown = set(files)
     
     for filepath in files:
-        cmdline.log.info("Now processing file '%s' ...", filepath)
+#        cmdline.log.info("Now processing file '%s' ...", filepath)
         inputfile = open(filepath, 'r')
 
         for foldername, spec in searches.iteritems():
@@ -205,11 +249,11 @@ def sepjobs(cmdline):
     # print summary
     for foldername, filenames in index.iteritems():
         print "== %s ==" % foldername, "no. of files:", len(index[foldername])
-        for filename in sorted(filenames):
-            print ("    " + filename)
+#        for filename in sorted(filenames):
+#            print ("    " + filename)
     print "== unknown ==", "no. of files:", len(unknown)
-    for filename in sorted(unknown):
-        print ("    " + filename)
+#    for filename in sorted(unknown):
+#        print ("    " + filename)
     cmdline.log.info("failure rate = %d, hit rate = %d", failrt, hitrt)
 
 
@@ -233,3 +277,20 @@ sepjobs.add_param('-S', '--search-root',
 
 if __name__ == '__main__':
         sepjobs.run()
+        
+""" 
+config file for sepjobs.py:
+
+# sample classifier file for sepjobs.py
+#
+# Each line has this form:
+#    
+#   searchstring: literal|regexp, foldername, [start, end], ['done']
+#
+               
+gracefully: regexp, gracefully, 90.0, done         
+
+I/O ERROR: literal, io_error, 80, 90, done, ,      
+ddikick.x: Timed out: literal, timed_out, 90, 99 , , ,
+Failed creating: literal, failed_creat, 20,,,,,,,
+"""
