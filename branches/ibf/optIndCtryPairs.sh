6267814f46309d11be2efda0ea6c2bf91cb5da26
benjamin.jonen@gmail.com <benjamin.jonen@gmail.com@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2011-08-06 11:54:49 +0000

    gParaSearch.py executable

--- branches/ibf/optIndCtryPairs.sh
+++ branches/ibf/optIndCtryPairs.sh
@@ -21,16 +21,29 @@ echo Please check markovB.in file
 sleep 1.5
 
 Ctrys=( AU CH CA DE FR JP UK US )
+nCtrys=${#Ctrys[@]}
 
+nCtry1=0
 for Ctry1 in ${Ctrys[@]}; do
+  nCtry1=$((nCtry1+1))
+  nCtry2=0
   for Ctry2 in ${Ctrys[@]}; do
+
+    nCtry2=$((nCtry2+1))
 
     echo "Ctry1: " $Ctry1
     echo "Ctry2: " $Ctry2
+    echo "nCtry1: " $nCtry1
+    echo "nCtry2: " $nCtry2
 
-    if [ $Ctry1 == $Ctry2 ]; then
-      echo "skipping diagnol"
-      continue
+    # if [ $Ctry1 == $Ctry2 ]; then
+    #   echo "skipping diagnol"
+    #   continue
+    # fi
+
+    if [ $nCtry1 -ge $nCtry2 ]; then
+	echo "only lower triangular. skipping.."
+	continue
     fi
 
     saveDir=${Ctry1}-${Ctry2}
@@ -55,7 +68,7 @@ for Ctry1 in ${Ctrys[@]}; do
     if ! [[ ${answer} =~ "y" ]]; then
       exit
     fi
-    /home/jonen/workspace/fpProj/model/code/gPremiumScripts/gParaSearch.py -b base -x /home/jonen/workspace/fpProj/model/bin/forwardPremiumOut -C 16 -N -NP 60 -xVars 'EA sigmaA' -xVarsDom '0.5 1.0 0.000 0.010' -sv info -t one4eachPair -e /home/jonen/workspace/fpProj/empirical/ --itermax 20 -yC 1.e-2 --countryList "${Ctry1} ${Ctry2}"
+    /home/jonen/workspace/fpProj/model/code/gPremiumScripts/gParaSearch.py -b base -x /home/jonen/workspace/fpProj/model/bin/forwardPremiumOut -C 16 -N -NP 100 -xVars 'EA sigmaA' -xVarsDom '0.5 1.0 0.000 0.010' -sv info -t one4eachPair -e /home/jonen/workspace/fpProj/empirical/ --itermax 20 -yC 1.e-2 --countryList "${Ctry1} ${Ctry2}"
      cd ..
   done
 done
