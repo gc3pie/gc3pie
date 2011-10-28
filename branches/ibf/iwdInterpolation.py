6a13b590b6b3dc1f808e0c1e67f08307d30ecd68
simon.scheuring@gmail.com <simon.scheuring@gmail.com@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2011-10-28 17:19:52 +0000

    added iwd

---
+++ branches/ibf/iwdInterpolation.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import os
+import numpy as np
+import scipy.optimize
+import scipy.interpolate as si
+import numpy.polynomial.chebyshev
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+
+from pymods.support.support import wrapLogger
+
+
+
+logger = wrapLogger(loggerName = 'costlyOptimizationLogger', streamVerb = 'DEBUG', logFile = os.path.join(os.getcwd(), 'costlyOpt.log'))
+
+np.seterr(all='raise')
+
+class iwdInterpolation(object):
+    '''
+    Inverse weighted density interpolation
+    '''
+    def __init__(self):
+        logger.debug('initializing new instance of iwdInterpolation')
+        self.x = np.array( np.array([]))
+        self.fx = np.array( [])
+
+    def addPoint(self, x, fx):
+        self.x = np.append(self.x, np.asarray(x))
+        self.fx = np.append(self.fx, np.asarray(fx))
+
+    def setPoints(self, x, fx):
+        self.x = x
+        self.fx = fx
+
+    def eval(self, x0):
+        sum_f0 = 0
+        sum_d = 0
+        for ix in range(0,len(self.x)):
+            dist = self.d(x0, self.x[ix])
+            sum_f0 = sum_f0 + dist * self.fx[ix]
+            sum_d = sum_d + dist
+        sum_f0 = sum_f0 / sum_d
+        return sum_f0
+
+    def d(self, x1, x2):
+        return 1.0 / np.linalg.norm(x1 - x2)
+
+if __name__ == '__main__':            
+    iwd = iwdInterpolation()
+
+    fun = lambda x: np.sum(np.log(x))
+    x1  = np.array([1, 2])
+    x2  = np.array([3, 5])
+    fx1 = fun(x1);
+    fx2 = fun(x2);
+
+    x   = np.array([x1, x2])
+    fx  = np.array([fx1, fx2])
+
+    print x
+    print fx
+
+    iwd.setPoints(x,fx)
+
+    print 'evaluation:'
+    print iwd.eval([2,4])
+ 
+    print 'exact value' 
+    print fun([2,4])
+    
+    print 'done'
+
