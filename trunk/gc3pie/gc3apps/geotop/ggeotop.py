2af5ba67147e1f598c4ad8af2b69ffa77f003b68
sergio.maffioletti <sergio.maffioletti@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2012-01-23 11:09:51 +0000

    Sergio: clean removes only defined patterns

--- trunk/gc3pie/gc3apps/geotop/ggeotop.py
+++ trunk/gc3pie/gc3apps/geotop/ggeotop.py
@@ -22,12 +22,30 @@ Front-end script for submitting multiple `GEOtop` jobs to SMSCG.
 It uses the generic `gc3libs.cmdline.SessionBasedScript` framework.
 
 See the output of ``ggeotop --help`` for program usage instructions.
+
+typical input folder:
+svf.asc
+asp.asc
+slp.asc
+dem.asc
+geotop.inpts
+rad/
+in/
+rec/
+maps/
 """
+
 __version__ = 'development version (SVN $Revision$)'
 # summary of user-visible changes
 __changelog__ = """
+
+  2012-01-19:
+  * Added mechanism to validate input folder on more detailed pattern
+  * compression of input done only on those folder/files known (as
+  opposite as before that was done with exclusion)
+
   2011-11-07:
-    * Initial release, forked off the ``gmhc_coev`` sources.
+  * Initial release, forked off the ``gmhc_coev`` sources.
 """
 __author__ = 'Riccardo Murri <riccardo.murri@uzh.ch>'
 __docformat__ = 'reStructuredText'
@@ -73,81 +91,44 @@ class GeotopApplication(Application):
 
     For more information about GEOtop, see <http://www.goetop.org/>
     """
+
+    def _scan_and_tar(self, simulation_dir):
+        try:
+            gc3libs.log.debug("Compressing input folder '%s'" % simulation_dir)
+            cwd = os.getcwd()
+            os.chdir(simulation_dir)
+            # check if input archive already present. If so, remove it
+            # XXX: improvement: add to the current archive only newer files from 'rec' folder
+            if os.path.isfile(GEOTOP_INPUT_ARCHIVE):
+                try:
+                    os.remove(GEOTOP_INPUT_ARCHIVE)
+                except OSError as x:
+                    gc3libs.log.error("Failed while removing %s. Type %s. Message %s" % (GEOTOP_INPUT_ARCHIVE, x.__class__, x.message))
+                    pass
+        
+            tar = tarfile.open(GEOTOP_INPUT_ARCHIVE, "w:gz")
+            tar.add('./in')
+            tar.add('./maps')
+            tar.add('./rec')
+            tar.add('./rad')
+            tar.add('./geotop.inpts')
+            tar.add('./svf.asc')
+            tar.add('./slp.asc')
+            tar.add('./asp.asc')
+            tar.add('./dem.asc')
+            tar.close()
+            os.chdir(cwd)
+            yield (tar.name, GEOTOP_INPUT_ARCHIVE)
+        except Exception as x:        
+            gc3libs.log.error("Failed while creating input archive. Error %s. Message %s" % (x.__class__,x.message))
+            raise
+
     def __init__(self, simulation_dir, executable=None, **kw):
         # remember for later
         self.simulation_dir = simulation_dir
-        # stage all (non-hidden) files in the simulation directory for input
 
-        def scan_and_tar(simulation_dir):
-            def geotop_exclude(filename):
-                return filename.endswith('~') \
-                       or filename.endswith('ggeotop.log') \
-                       or filename.endswith('_static') \
-                       or filename.endswith('geotop.log') \
-                       or filename.endswith(GEOTOP_OUTPUT_ARCHIVE) \
-                       or filename.endswith(GEOTOP_INPUT_ARCHIVE) \
-                       or filename.startswith('./.arc')        
+        inputs = dict((a,b) for (a,b) in self._scan_and_tar(simulation_dir))
 
-            try:
-                gc3libs.log.debug("Compressing input folder '%s'" % simulation_dir)
-                cwd = os.getcwd()
-                os.chdir(simulation_dir)
-                # check if input archive already present. If so, remove it
-                if os.path.isfile(GEOTOP_INPUT_ARCHIVE):
-                    try:
-                        os.remove(GEOTOP_INPUT_ARCHIVE)
-                    except OSError as x:
-                        gc3libs.log.error("Failed while removing %s. Type %s. Message %s" % (GEOTOP_INPUT_ARCHIVE, x.__class__, x.message))
-                        pass
-
-                tar = tarfile.open(GEOTOP_INPUT_ARCHIVE, "w:gz")
-                tar.add('.',recursive=True,exclude=geotop_exclude)
-                tar.close()
-                os.chdir(cwd)
-                yield (tar.name, GEOTOP_INPUT_ARCHIVE)
-            except:
-                raise
-
-        # def walk_input_folder(simulation_dir):
-        #     exclude_patterns = ['~','.arc','/in']
-        #     for r,d,f in os.walk(simulation_dir):
-        #         if r.endswith('~') or r.endswith('.arc') or r.endswith('/in'):
-        #             continue
-        #         for filename in f:
-        #             # full_filename = os.path.join(r,filename)
-        #             # remote_entry_filename = full_filename.split(simulation_dir)[1]
-        #             yield (os.path.join(r,filename),"")
-            
-        # def fill_empty_folder(simulation_dir):
-        #     for dirpath, dirnames, filenames in os.walk(simulation_dir):
-        #         entry = os.path.basename(dirpath)
-        #         if not entry.endswith('~'):
-        #             if not dirnames and not filenames:
-        #                 # Folder is empty; fill it with a 'placeholder' file
-        #                 try:
-        #                     f = open(os.path.join(dirpath, GC3PIE_PLACEHOLDER_FILENAME),"w+")
-        #                     f.close()
-        #                     yield ((os.path.join(dirpath, GC3PIE_PLACEHOLDER_FILENAME),
-        #                             os.path.join(entry, GC3PIE_PLACEHOLDER_FILENAME)))
-        #                 except IOError:
-        #                     raise
-        #             else:
-        #                 for f in filenames:
-        #                     if dirpath == simulation_dir:
-        #                         yield(os.path.join(dirpath,f),f)
-        #                     else:
-        #                         yield (os.path.join(dirpath,f),os.path.join(entry,f))
-
-
-        # inputs = dict((a,b) for (a,b) in fill_empty_folder(simulation_dir))
-
-        inputs = dict((a,b) for (a,b) in scan_and_tar(simulation_dir))
-
-        ## XXX: convention is that 'in' folder contains only input values that should not
-        ## be retrieved as part of the output
-        # outputs = {}
-        # outputs[GEOTOP_OUTPUT_ARCHIVE] = GEOTOP_OUTPUT_ARCHIVE
-        
         geotop_wrapper_sh = resource_filename(Requirement.parse("gc3pie"),
                                               "gc3libs/etc/geotop_wrap.sh")
 
@@ -162,11 +143,12 @@ class GeotopApplication(Application):
             # use the default one provided by the RTE
             executable_name = '/$GEOTOP'
 
-        # outputs = {}
-        # outputs[GEOTOP_OUTPUT_ARCHIVE] = GEOTOP_OUTPUT_ARCHIVE
         # set some execution defaults...
         kw.setdefault('requested_cores', 1)
         kw.setdefault('requested_architecture', Run.Arch.X86_64)
+
+        # XXX: this will have to be adjusted
+        # this should come from the command line interface
         kw.setdefault('requested_walltime',16)
         # ...and remove excess ones
         kw.pop('output_dir', None)
@@ -205,7 +187,7 @@ class GeotopApplication(Application):
 
         # check and unpack output archive
         if os.path.isfile(full_tarname):
-            # execution should have been completed sucessfully
+            # execution somehow terminated.
             # untar archive
             gc3libs.log.info("Expected output archive found in %s" % full_tarname)
             
@@ -225,18 +207,6 @@ class GeotopApplication(Application):
             self.stderr,
             ]
 
-        # # move files one level up
-        # # subprocess.call(args, *, stdin=None, stdout=None, stderr=None, shell=False)
-        # try:
-        #     for entry in os.listdir(tmp_output_dir):
-        #         dest_entry = os.path.join(self.simulation_dir, entry)
-        #         _command = "mv --update %s/* %s/" % (entry,dest_dir)
-        #         _command = shlex.split(_command)
-        #         subprocess.call(_command)
-        # except Exception, x:
-        #     gc3libs.log.error("Failed running %s. Error type %s. Message %s" % (_command,x.__class__,x.message))
-        #     pass
-    
         # move files one level up, except the ones listed in `exclude`
         for entry in os.listdir(tmp_output_dir):
             src_entry = os.path.join(tmp_output_dir, entry)
@@ -255,14 +225,7 @@ class GeotopApplication(Application):
                 # delete entry and continue with next one
                 os.remove(src_entry)
                 continue
-            # # special files indicate successful or unsuccessful completion
-            # if entry in [ '_SUCCESSFUL_RUN', '_SUCCESSFUL_RUN.old' ]:
-            #     # if .R then run 'R CMD <filename>.R' on the .R
-            #     # self._execute_postprocess()
-            #     self.execution.returncode = (0, posix.EX_OK)
-            # elif entry in [ '_FAILED_RUN', '_FAILED_RUN.old' ]:
-            #     # use exit code 100 to indicate total failure
-            #     self.execution.returncode = (0, 100)
+
             # now really move file one level up
             dest_entry = os.path.join(self.simulation_dir, entry)
             if os.path.exists(dest_entry):
@@ -279,7 +242,14 @@ class GeotopApplication(Application):
         elif os.path.isfile(os.path.join(self.simulation_dir,'_FAILED_RUN')) or os.path.isfile(os.path.join(self.simulation_dir,'_FAILED_RUN.old')):
             # use exit code 100 to indicate total failure
             self.execution.returncode = (0, 100)
-
+        else:
+            # should be resubmitted
+            # call _scan_and_tar to create new archive
+            # needed to include 'rec' folder retrieved from previous execution
+            # XXX: To consider a better way of handling this
+            # at the moment the entire input archive is recreated
+            gc3libs.log.info("Updating tar archive for resubmission")
+            self._scan_and_tar(self.simulation_dir)
 
 class GeotopTask(RetryableTask, gc3libs.utils.Struct):
 
@@ -346,6 +316,9 @@ newly-created jobs so that this limit is never exceeded.
         self.add_param("-x", "--executable", metavar="PATH", #type=executable_file,
                        dest="executable", default=None,
                        help="Path to the GEOtop executable file.")
+        self.add_param("-q", "--query", action="store_true", dest="query_only",
+                       help="Do not submit jobs, just validate input folders.")
+
         # change default for the "-o"/"--output" option
         #self.actions['output'].default = 'NPOPSIZE/PARAMS/ITERATION'
 
@@ -370,27 +343,70 @@ newly-created jobs so that this limit is never exceeded.
         input_files = self._search_for_input_files(self.params.args, 'geotop.inpts')
 
         # the real input to GEOtop are the directories containing `geotop.inpts`
-        input_dirs = [ (os.path.dirname(path) or os.getcwd())
-                       for path in input_files ]
+        # input_dirs = [ (os.path.dirname(path) or os.getcwd())
+        #               for path in input_files ]
 
-        for path in input_dirs:
-            # construct GEOtop job
-            yield (
-                # job name
-                gc3libs.utils.basename_sans(path),
-                # task constructor
-                ggeotop.GeotopTask,
-                [ # parameters passed to the constructor, see `GeotopTask.__init__`
-                    path,                   # path to the directory containing input files
-                    self.params.executable, # path to the GEOtop executable
-                ],
-                # extra keyword arguments passed to the constructor,
-                # see `GeotopTask.__init__`
-                extra.copy()
-                )
+        for path in self._validate_input_folder(input_files):
+            if self.params.query_only:
+                print("Valid input folder '%s'" % path)
+            else:
+                # construct GEOtop job
+                yield (
+                    # job name
+                    gc3libs.utils.basename_sans(path),
+                    # task constructor
+                    ggeotop.GeotopTask,
+                    [ # parameters passed to the constructor, see `GeotopTask.__init__`
+                        path,                   # path to the directory containing input files
+                        self.params.executable, # path to the GEOtop executable
+                    ],
+                    # extra keyword arguments passed to the constructor,
+                    # see `GeotopTask.__init__`
+                    extra.copy()
+                    )
 
+    def _validate_input_folder(self, inputfiles):
+        """
+        Checks if, provided a valida input filename of type
+        'geotop.inpts', the corresponding folder contains the required
+        additional files and folders:
         
+        ./in
+        ./maps
+        ./rec
+        ./rad
+        ./geotop.inpts
+        ./svf.asc
+        ./slp.asc
+        ./asp.asc
+        ./dem.asc
+
+        /out folders are excluded
+
+        @input: list of filenamens
+        @output: iterator of 'valid' input_dirs
+        
+        """
+
+        for input_filename in inputfiles:
+            # extract folder name from input_filename
+            dirname = os.path.dirname(input_filename)
+            if os.path.isdir(os.path.join(dirname,"in")) and \
+                   os.path.isdir(os.path.join(dirname,"maps")) and \
+                   os.path.isdir(os.path.join(dirname,"rec")) and \
+                   os.path.isdir(os.path.join(dirname,"rad")) and \
+                   os.path.isfile(os.path.join(dirname,"geotop.inpts")) and \
+                   os.path.isfile(os.path.join(dirname,"svf.asc")) and \
+                   os.path.isfile(os.path.join(dirname,"slp.asc")) and \
+                   os.path.isfile(os.path.join(dirname,"asp.asc")) and \
+                   os.path.isfile(os.path.join(dirname,"dem.asc")) and \
+                   not dirname.endswith("/out") and \
+                   not dirname.endswith("~"):
+                yield dirname
+            else:
+                gc3libs.log.warning("validate_input_folder skipping '%s'" %  dirname)
 
 # run it
 if __name__ == '__main__':
     GGeotopScript().run()
+
