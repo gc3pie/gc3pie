df77e13c9caff4d4de35df3745f3adec9783d8cf
arcimboldo@gmail.com <arcimboldo@gmail.com@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2012-03-08 09:05:21 +0000

    Make it "work" with py.test and nosetests
    running py.test or nosetests will try to execute this program which
    unfortunately doesn't work since it needs an argument to be passed
    from the command line. Putting the main code inside the if __name__ ==
    "__main__" statement will make it work as before and not disturbing
    nosetests/py.test at the same time.
    
    	modified:   gc3pie/test/test_nested_persistence_load.py

--- trunk/gc3pie/test/test_nested_persistence_load.py
+++ trunk/gc3pie/test/test_nested_persistence_load.py
@@ -11,20 +11,23 @@ class MyList(list):
     """
     pass
 
-ls = fs.load(sys.argv[1])
-#print ls
+if __name__ == "__main__":
+    ls = fs.load(sys.argv[1])
+    #print ls
 
-try:
-    if ls[0].my_note != 'This is j1':
-        raise Error
-    if ls[1].my_note != 'This is j2':
-        raise Error
-    if ls[2].my_other_note != 'Yet another note.':
-        raise Error
-except:
-    print 'FAIL'
-    sys.exit(1)
+    try:
+        if ls[0].my_note != 'This is j1':
+            raise Error
+        if ls[1].my_note != 'This is j2':
+            raise Error
+        if ls[2].my_other_note != 'Yet another note.':
+            raise Error
+    except:
+        print 'FAIL'
+        sys.exit(1)
 
-# if we get here, all is fine and well
-print 'OK'
-sys.exit(0)
+    # if we get here, all is fine and well
+    print 'OK'
+    sys.exit(0)
+
+
