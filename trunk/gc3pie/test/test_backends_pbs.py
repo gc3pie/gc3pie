da3cfaa2e38683d2868978058c02a7b9cf1be410
arcimboldo@gmail.com <arcimboldo@gmail.com@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2012-03-14 21:22:35 +0000

    PBS unit tests improved
    The faketransport.FakeTransport class is intended to be used as a Mock Class to test various backends.
    The PBS backend tests now use this class.

--- trunk/gc3pie/test/test_backends_pbs.py
+++ trunk/gc3pie/test/test_backends_pbs.py
@@ -21,19 +21,69 @@
 __docformat__ = 'reStructuredText'
 __version__ = '$Revision$'
 
-qsub_out = """35488.argo.ictp.it
-"""
 
-qstat_notfound_err = """qstat: Unknown Job Id %s
-"""
+def correct_submit(jobid=123):
+    out = """%d.argo.ictp.it
+    """ % jobid
+    return (0, out, "")
 
-qstat_queued_out = """36236.argo                 antani     amessina                0 Q short
+def tracejob_notfound(jobid=123):
+    err = """/var/spool/torque/server_priv/accounting/20120309: Permission denied
+/var/spool/torque/mom_logs/20120309: No such file or directory
+/var/spool/torque/sched_logs/20120309: No such file or directory
 """
-qstat_running_out = """36248.argo                 cam_icbc         idiallo         01:01:18 R short
-"""
+    
+    return (0, "", err)
 
-tracejob_out = """
-Job: 36174.argo.ictp.it
+def correct_qstat_queued(jobid=123):
+    out = """%d.argo                 antani     amessina                0 Q short
+""" % jobid
+    return (0, out, "")
+
+def correct_tracejob_queued(jobid=123):
+    out = """Job: %d.argo.ictp.it
+
+03/14/2012 18:42:04  S    enqueuing into short, state 1 hop 1
+03/14/2012 18:42:04  S    Job Queued at request of amessina@argo.ictp.it, owner = amessina@argo.ictp.it, job name = STDIN, queue = short
+""" % jobid
+    err = """/var/spool/torque/server_priv/accounting/20120314: Permission denied
+/var/spool/torque/mom_logs/20120314: No such file or directory
+/var/spool/torque/sched_logs/20120314: No such file or directory
+"""
+    return (0, out, err)
+
+
+def correct_qstat_running(jobid=123):
+    out = """%d.argo                 cam_icbc         idiallo         01:01:18 R short
+""" % jobid
+    return (0, out, "")
+
+def correct_tracejob_running(jobid=123):
+    out = """Job: %d.argo.ictp.it
+
+03/14/2012 18:42:04  S    enqueuing into short, state 1 hop 1
+03/14/2012 18:42:04  S    Job Queued at request of amessina@argo.ictp.it, owner = amessina@argo.ictp.it, job name = STDIN, queue = short
+03/14/2012 18:45:25  S    Job Run at request of root@argo.ictp.it
+03/14/2012 18:45:25  S    Not sending email: User does not want mail of this type.
+""" % jobid
+    err = """/var/spool/torque/server_priv/accounting/20120314: Permission denied
+/var/spool/torque/mom_logs/20120314: No such file or directory
+/var/spool/torque/sched_logs/20120314: No such file or directory
+"""
+    return (0, out, err)
+
+
+def qdel_notfound(jobid=123):
+    return (153,"",  """qdel: Unknown Job Id %d.argo.ictp.it""" % jobid)
+
+def qstat_notfound(jobid=123):
+    err = """qstat: Unknown Job Id %d
+""" % jobid
+    return (153, "", err)
+
+def correct_tracejob_done(jobid=123):
+    out = """
+Job: %d.argo.ictp.it
 
 03/09/2012 09:31:53  S    enqueuing into short, state 1 hop 1
 03/09/2012 09:31:53  S    Job Queued at request of amessina@argo.ictp.it, owner = amessina@argo.ictp.it, job name = DemoPBSApp, queue = short
@@ -42,12 +92,27 @@ Job: 36174.argo.ictp.it
 03/09/2012 09:34:08  S    Not sending email: User does not want mail of this type.
 03/09/2012 09:34:08  S    Exit_status=0 resources_used.cput=00:00:00 resources_used.mem=2364kb resources_used.vmem=190944kb resources_used.walltime=00:02:05
 03/09/2012 09:34:08  S    dequeuing from short, state COMPLETE
-"""
+""" % jobid
 
-tracejob_err = """/var/spool/torque/server_priv/accounting/20120309: Permission denied
+    err = """/var/spool/torque/server_priv/accounting/20120309: Permission denied
 /var/spool/torque/mom_logs/20120309: No such file or directory
 /var/spool/torque/sched_logs/20120309: No such file or directory
 """
+    
+    return (0, out, err)
+
+
+def qsub_failed_resources():
+    out = ""
+    err = """qsub: Job exceeds queue resource limits MSG=cannot locate feasible nodes
+"""
+    return (190, out, err)
+
+def qsub_failed_acl():
+    out = ""
+    err = """qsub: Unauthorized Request  MSG=user ACL rejected the submitting user: user amessina@argo.ictp.it, queue cm1
+"""
+    return (159, out, err)
 
 from gc3libs.Resource import Resource
 from gc3libs.authentication import Auth
@@ -55,48 +120,8 @@ import gc3libs, gc3libs.core
 #import gc3libs.Run.State as State
 State = gc3libs.Run.State
 
-from gc3libs.backends.transport import LocalTransport
+from faketransport import FakeTransport
 
-class FakePBSTransport(LocalTransport):
-    def __init__(self):
-        LocalTransport.__init__(self)        
-        self.status = State.NEW
-        
-    def set_job_status(self, status):
-        self.status = status
-    
-    def execute_command(self, command):
-        """parse the command and return fake output and error codes
-        depending on the current suppose status of the job"""
-        commands = []
-        # this is a excessively complex routine to split a command like "cmd1 ; cmd2 && cmd3 || cmd4"
-        
-        _ = command.split(';')
-        for i in _:
-            commands.extend(i.strip().split('&&'))
-        
-        for cmd in commands:
-            args = cmd.strip().split()
-            if args[0] == 'qsub':
-                return (0, qsub_out, "")
-            elif args[0] == 'tracejob':
-                # Job is done, returning output
-                return (0, tracejob_out, tracejob_err)
-            elif args[0] == 'qstat':
-                if len(args)==5 and args[2] == '|' and args[3] == 'grep':
-                    # qstat jobid | grep ^jobid
-                    if self.status == State.RUNNING:
-                        return (0, qstat_running_out, "")
-                    elif self.status == State.SUBMITTED:
-                        return (0, qstat_queued_out, "")
-                    elif self.status == State.TERMINATING:
-                        return (153, "", qstat_notfound_err % args[1])
-            elif args[0] == 'tracejob':
-                    return (0, tracejob_out, tracejob_err)
-            else:
-                continue
-        return LocalTransport.execute_command(self, command)
-    
 def _setup_conf():
     resource = Resource(**{
     'name' : 'argo',
@@ -134,32 +159,67 @@ class FakeApp(gc3libs.Application):
             stderr = "stderr.txt",
             requested_cores = 1,)
 
-
-
-def test_pbs_basic_workflow():
+def _common_setup():
     g = _setup_core()
     b = g.get_backend('argo')
-    b.transport = FakePBSTransport()
-
+    b.transport = FakeTransport()
+    t = b.transport
+    # Basic responses
+    t.expected_answer['qstat'] = qstat_notfound()
+    t.expected_answer['tracejob'] = tracejob_notfound()
+    t.expected_answer['qdel'] = qdel_notfound()
+    
     app = FakeApp()
+    return (g,t, app)
+
+def test_submission_failed():
+    (g, t, app)  = _common_setup()
+
+    # Submission failed (unable to find resources):
+    t.expected_answer['qsub'] = qsub_failed_resources()
+    try:
+        g.submit(app)
+    except Exception, e:
+        assert isinstance(e, gc3libs.exceptions.LRMSError)
+    assert app.execution.state == State.NEW
+
+    # Submission failed (unauthrozed user):
+    t.expected_answer['qsub'] = qsub_failed_acl()
+    try:
+        g.submit(app)
+    except Exception, e:
+        assert isinstance(e, gc3libs.exceptions.LRMSError)
+    assert app.execution.state == State.NEW
+
+    
+def test_pbs_basic_workflow():
+    (g, t, app)  = _common_setup()
+
+    # Succesful submission:
+    t.expected_answer['qsub'] = correct_submit()
     g.submit(app)
-    b.transport.set_job_status(app.execution.state)
     assert app.execution.state == State.SUBMITTED
 
-    g.update_job_state(app)
-    b.transport.set_job_status(app.execution.state)
-    
-    b.transport.set_job_status(State.RUNNING)
-    g.update_job_state(app)
-    b.transport.set_job_status(app.execution.state)
-    assert app.execution.state == State.RUNNING
-    
-    b.transport.set_job_status(State.TERMINATING)
-    g.update_job_state(app)
-    b.transport.set_job_status(app.execution.state)
-    assert app.execution.state == State.TERMINATING
 
+    # Update state. We would expect the job to be SUBMITTED
+    t.expected_answer['qstat'] = correct_qstat_queued()
+    t.expected_answer['tracejob'] = correct_tracejob_queued()
+    g.update_job_state(app)
+    assert app.execution.state == State.SUBMITTED
+
+    # Update state. We would expect the job to be RUNNING
+    t.expected_answer['qstat'] = correct_qstat_running()
+    t.expected_answer['tracejob'] = correct_tracejob_running()    
+    g.update_job_state(app)
+    assert app.execution.state == State.RUNNING
+
+    # Job done. qstat doesn't find it, tracejob should.
+    t.expected_answer['qstat'] = qstat_notfound()
+    t.expected_answer['tracejob'] = correct_tracejob_done()    
+    g.update_job_state(app)
+    assert app.execution.state == State.TERMINATING
     
 
 if __name__ == "__main__":
     test_pbs_basic_workflow()
+    test_submission_failed()
