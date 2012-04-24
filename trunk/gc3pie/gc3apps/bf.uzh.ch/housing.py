49fe76d967900cce080316cbcb05d21b3ba9de1d
benjamin.jonen <benjamin.jonen@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2012-04-24 09:33:39 +0000

    housing.py: walltime set manually from now on

--- trunk/gc3pie/gc3apps/bf.uzh.ch/housing.py
+++ trunk/gc3pie/gc3apps/bf.uzh.ch/housing.py
@@ -54,7 +54,7 @@ class housingApplication(Application):
     _invalid_chars = re.compile(r'[^_a-zA-Z0-9]+', re.X)
     
     def __init__(self, executable, arguments, inputs, outputs, output_dir, **kw):
-        kw.setdefault('requested_walltime', 2)
+#        kw.setdefault('requested_walltime', 2)
         Application.__init__(self, executable, arguments, inputs, outputs, output_dir, **kw)
     
     def fetch_output_error(self, ex):
@@ -154,6 +154,6 @@ class housingApplication(Application):
 class housingApppotApplication(housingApplication, gc3libs.application.apppot.AppPotApplication):
     _invalid_chars = re.compile(r'[^_a-zA-Z0-9]+', re.X)
     def __init__(self, executable, arguments, inputs, outputs, output_dir, **kw):
-        kw.setdefault('requested_walltime', 2)
+#        kw.setdefault('requested_walltime', 2) # unnecessary.. gc3pie automatically sets default to 8
         gc3libs.application.apppot.AppPotApplication.__init__(self, executable, arguments, inputs, outputs, output_dir, **kw)
 
