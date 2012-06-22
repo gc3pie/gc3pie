34996cddf5d183a6069cf8723755b6161a6f0c56
arcimboldo@gmail.com <arcimboldo@gmail.com@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2012-06-22 08:28:28 +0000

    Create the `finished` file inside session dir when the session is complete
    	modified:   gc3libs/cmdline.py

--- trunk/gc3pie/gc3libs/cmdline.py
+++ trunk/gc3pie/gc3libs/cmdline.py
@@ -1317,6 +1317,9 @@ class SessionBasedScript(_Script):
                 pass
         self.after_main_loop()
 
+        # Set the end timestamp in the session directory
+        self.session.set_end_timestamp()
+
         # save the session again before exiting, so the file reflects
         # jobs' statuses
         self.session.save_all()
