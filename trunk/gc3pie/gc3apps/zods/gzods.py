7828b7e69881bf3f08bea29fa19201f0446790ba
lukasz.miroslaw@gmail.com <lukasz.miroslaw@gmail.com@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2012-07-21 19:38:52 +0000

    Fixed a bug. Now all the output ZODS files can be retrieved.

--- trunk/gc3pie/gc3apps/zods/gzods.py
+++ trunk/gc3pie/gc3apps/zods/gzods.py
@@ -21,20 +21,23 @@ class GzodsApp(gc3libs.Application):
     This class is derived from gc3libs.Application and defines ZODS app with its input and output files. 
     """
     def __init__(self, filename,**kw):
-        if self.check_input(filename) == None:
+	if self.check_input(filename) == None:
 		gc3libs.log.warning("Input files for ZODS app was not detected.")
 		return None
 	(input1, input2) = self.check_input(filename)
-        
-	gc3libs.log.debug("Detected input file for ZODS: %s", filename)
+	input1 = os.path.abspath(input1)
+	input2 = os.path.abspath(input2)
+	filename = filename
+	#filename = os.path.abspath(filename)
+	gc3libs.log.debug("Detected input files for ZODS: %s, %s and %s.", filename, input1, input2)
 	gc3libs.Application.__init__(
             self,
             tags=["APPS/CHEM/ZODS-01"],
 	    executable = '$MPIRUN', # mandatory
             arguments = ["-n", kw['requested_cores'], '$ZODS_BINDIR/simulator',filename],                
             inputs = [filename, input1, input2],    # mandatory, inputs are files that will be copied to the site
-            outputs = ['zods.txt'],                 # mandatory
-            stderr = "stderr.txt",
+            outputs = gc3libs.ANY_OUTPUT,                 # mandatory
+	    stderr = "stderr.txt",
 	    stdout = "stdout.txt",
 	    **kw	)
 
@@ -42,7 +45,8 @@ class GzodsApp(gc3libs.Application):
     def terminated(self):
         filename = os.path.join(self.output_dir, self.stdout)
         gc3libs.log.debug("ZODS single job based on %s has TERMINATED", filename)
-
+	for output in self.outputs:
+		gc3libs.log.debug("Retrieved the following file from ZODS job %s", output) 
 
 # Detect the following references to external files in input.xml
 #   <average_structure>
@@ -76,20 +80,23 @@ class GzodsApp(gc3libs.Application):
 class ZodsScript(gc3libs.cmdline.SessionBasedScript):
 	"""
 Thiss application will run ZODS app on distributed resources.
-The script scans the specified INPUTDIR directories recursively for '.xml' files,
+The script scans the specified INPUTDIR directories recursively for 'input*.xml' files,
 and submits a ZODS job for each input file found; job progress is
 monitored and, when a job is done, its output files are retrieved back
 into the same directory where the '.xml' file is.
 Example run on 5 cores:
-./gzods dirWithInputFiles -c 5
+./gzods input.xml -c 5
+TODO: right now only input file is accepted as an argument. Directories are not accepted because ZODS does not run when the full path to the input file is provided.
+TODO: in new_tasks() the program should exit when the argument list is correct. Below self.params.args is empty, None object is returned and gc3pie is still running
 	"""
 	version = "1.0"
 
 	def new_tasks(self, extra):
 	   tasks=[]
-	   if len(self.params.args) == 0:
- 	   	self.log.info("Please specify the directory with input files")
-		return 		
+           #self.output_dir = os.path.relpath(kw.get('output_dir'))
+	   if self.params.args is None or len(self.params.args) == 0:
+ 	   	self.log.warning("Please specify the directory with input files")
+		return None		
  	   self.log.info("Analayzed dirs =%s", self.params.args)
 	   listOfFiles = self._search_for_input_files(self.params.args, pattern="input*.xml")
 	   gc3libs.log.debug("List of detected input files for ZODS: %s", listOfFiles) 
