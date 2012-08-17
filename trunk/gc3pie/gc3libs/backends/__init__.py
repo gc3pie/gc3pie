e8296b992fcf833ef6d1a23ed8969083752cf0a1
arcimboldo@gmail.com <arcimboldo@gmail.com@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2012-08-17 08:45:06 +0000

    Version 2.0.0-a1

--- trunk/gc3pie/gc3libs/backends/__init__.py
+++ trunk/gc3pie/gc3libs/backends/__init__.py
@@ -19,7 +19,7 @@ Interface to different resource management systems for the GC3Libs.
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
 __docformat__ = 'reStructuredText'
-__version__ = 'development version (SVN $Revision$)'
+__version__ = '2.0.0-a1 version (SVN $Revision$)'
 
 
 import gc3libs
