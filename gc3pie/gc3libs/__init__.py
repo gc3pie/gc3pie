49ecad3ff649fbfc36d4e48a669031265c23e209
arcimboldo <arcimboldo@gmail.com>
2012-09-30 14:10:50 +0000

    Fix infinite recursion when calling Task.free()

--- gc3pie/gc3libs/__init__.py
+++ gc3pie/gc3libs/__init__.py
@@ -362,10 +362,7 @@ class Task(Persistable, Struct):
 
         See :meth:`gc3libs.Core.free` for a full explanation.
         """
-        assert self._attached, ("Task.free() called on detached task %s." % self)
-        assert hasattr(self._controller, 'free'), \
-               ("Invalid `_controller` object '%s' in Task %s" % (self._controller, self))
-        self._controller.free(self, **extra_args)
+        return
 
 
     # convenience methods, do not really add any functionality over
