5e63739a7a2b0a2554e5dc3e3e70dee02788a320
riccardo.murri <riccardo.murri@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2012-09-30 15:28:26 +0000

    Tagged branch 'branches/2.0' to 'tags/2.0.0-rc2'

--- gc3pie/gc3apps/zods/gzods.py
+++ gc3pie/gc3apps/zods/gzods.py
@@ -23,7 +23,7 @@ It uses the generic `gc3libs.cmdline.SessionBasedScript` framework.
 
 See the output of ``gzods --help`` for program usage instructions.
 """
-__version__ = 'development version (SVN $Revision$)'
+__version__ = '2.0.0-rc2 version (SVN $Revision$)'
 # summary of user-visible changes
 __changelog__ = """
   2012-07-17:
--- gc3pie/gc3libs/authentication/grid.py
+++ gc3pie/gc3libs/authentication/grid.py
@@ -20,7 +20,7 @@ Authentication support with Grid proxy certificates.
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
 __docformat__ = 'reStructuredText'
-__version__ = 'development version (SVN $Revision$)'
+__version__ = '2.0.0-rc2 version (SVN $Revision$)'
 
 import errno
 import getpass
