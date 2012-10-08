237ad6b599a7e0d2a738dab3eb23ac6f897b4741
arcimboldo <arcimboldo@gmail.com>
2012-10-08 11:42:56 +0000

    Add back _collections.py that disappeared from the source code.
    Fixes issue 343.
    
    	new file:   _collections.py

---
+++ gc3pie/gc3libs/compat/_collections.py
@@ -0,0 +1,409 @@
+"""
+A backport of the Python standard `collections` package, providing
+`namedtuple` and `defaultdict` also on Python 2.4 and 2.5.
+
+This package actually imports your Python `collections`, and adds
+its own versions of `namedtuple` and `defaultdict` only if they are
+missing.
+"""
+
+from collections import *
+
+try:
+    defaultdict
+except NameError:
+    class defaultdict(dict):
+        """
+        A backport of `defaultdict` to Python 2.4
+        See http://docs.python.org/library/collections.html
+
+        >>> dl = defaultdict(list)
+        >>> dl['emptylist']
+        []
+        >>> di = defaultdict(int)
+        >>> di['zero']
+        0
+        >>> ds = defaultdict(str)
+        >>> ds['empty']
+        ''
+
+        """
+        def __new__(cls, default_factory=None):
+            return dict.__new__(cls)
+        def __init__(self, default_factory):
+            self.default_factory = default_factory
+        def __missing__(self, key):
+            try:
+                return self.default_factory()
+            except:
+                raise KeyError("Key '%s' not in dictionary" % key)
+        def __getitem__(self, key):
+            if not dict.__contains__(self, key):
+                dict.__setitem__(self, key, self.__missing__(key))
+            return dict.__getitem__(self, key)
+
+
+try:
+    namedtuple
+except NameError:
+    # Use Raymond Hettinger's original `namedtuple` package.
+    #
+    # Source originally taken from:
+    # http://code.activestate.com/recipes/500261-named-tuples/
+    from operator import itemgetter as _itemgetter
+    from keyword import iskeyword as _iskeyword
+    import sys as _sys
+
+    def namedtuple(typename, field_names, verbose=False, rename=False):
+        """Returns a new subclass of tuple with named fields.
+
+        >>> Point = namedtuple('Point', 'x y')
+        >>> Point.__doc__                   # docstring for the new class
+        'Point(x, y)'
+        >>> p = Point(11, y=22)             # instantiate with positional args or keywords
+        >>> p[0] + p[1]                     # indexable like a plain tuple
+        33
+        >>> x, y = p                        # unpack like a regular tuple
+        >>> x, y
+        (11, 22)
+        >>> p.x + p.y                       # fields also accessable by name
+        33
+        >>> d = p._asdict()                 # convert to a dictionary
+        >>> d['x']
+        11
+        >>> Point(**d)                      # convert from a dictionary
+        Point(x=11, y=22)
+        >>> p._replace(x=100)               # _replace() is like str.replace() but targets named fields
+        Point(x=100, y=22)
+
+        """
+
+        # Parse and validate the field names.  Validation serves two purposes,
+        # generating informative error messages and preventing template injection attacks.
+        if isinstance(field_names, basestring):
+            field_names = field_names.replace(',', ' ').split() # names separated by whitespace and/or commas
+        field_names = tuple(map(str, field_names))
+        if rename:
+            names = list(field_names)
+            seen = set()
+            for i, name in enumerate(names):
+                if (not min(c.isalnum() or c=='_' for c in name) or _iskeyword(name)
+                    or not name or name[0].isdigit() or name.startswith('_')
+                    or name in seen):
+                        names[i] = '_%d' % i
+                seen.add(name)
+            field_names = tuple(names)
+        for name in (typename,) + field_names:
+            if not min(c.isalnum() or c=='_' for c in name):
+                raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r' % name)
+            if _iskeyword(name):
+                raise ValueError('Type names and field names cannot be a keyword: %r' % name)
+            if name[0].isdigit():
+                raise ValueError('Type names and field names cannot start with a number: %r' % name)
+        seen_names = set()
+        for name in field_names:
+            if name.startswith('_') and not rename:
+                raise ValueError('Field names cannot start with an underscore: %r' % name)
+            if name in seen_names:
+                raise ValueError('Encountered duplicate field name: %r' % name)
+            seen_names.add(name)
+
+        # Create and fill-in the class template
+        numfields = len(field_names)
+        argtxt = repr(field_names).replace("'", "")[1:-1]   # tuple repr without parens or quotes
+        reprtxt = ', '.join('%s=%%r' % name for name in field_names)
+        template = '''class %(typename)s(tuple):
+            '%(typename)s(%(argtxt)s)' \n
+            __slots__ = () \n
+            _fields = %(field_names)r \n
+            def __new__(_cls, %(argtxt)s):
+                return _tuple.__new__(_cls, (%(argtxt)s)) \n
+            @classmethod
+            def _make(cls, iterable, new=tuple.__new__, len=len):
+                'Make a new %(typename)s object from a sequence or iterable'
+                result = new(cls, iterable)
+                if len(result) != %(numfields)d:
+                    raise TypeError('Expected %(numfields)d arguments, got %%d' %% len(result))
+                return result \n
+            def __repr__(self):
+                return '%(typename)s(%(reprtxt)s)' %% self \n
+            def _asdict(self):
+                'Return a new dict which maps field names to their values'
+                return dict(zip(self._fields, self)) \n
+            def _replace(_self, **kwds):
+                'Return a new %(typename)s object replacing specified fields with new values'
+                result = _self._make(map(kwds.pop, %(field_names)r, _self))
+                if kwds:
+                    raise ValueError('Got unexpected field names: %%r' %% kwds.keys())
+                return result \n
+            def __getnewargs__(self):
+                return tuple(self) \n\n''' % locals()
+        for i, name in enumerate(field_names):
+            template += '            %s = _property(_itemgetter(%d))\n' % (name, i)
+        if verbose:
+            print template
+
+        # Execute the template string in a temporary namespace
+        namespace = dict(_itemgetter=_itemgetter, __name__='namedtuple_%s' % typename,
+                         _property=property, _tuple=tuple)
+        try:
+            exec template in namespace
+        except SyntaxError, e:
+            raise SyntaxError(str(e) + ':\n' + template)
+        result = namespace[typename]
+
+        # For pickling to work, the __module__ variable needs to be set to the frame
+        # where the named tuple is created.  Bypass this step in enviroments where
+        # sys._getframe is not defined (Jython for example) or sys._getframe is not
+        # defined for arguments greater than 0 (IronPython).
+        try:
+            result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+        except (AttributeError, ValueError):
+            pass
+
+        return result
+
+try:
+    OrderedDict
+except:
+    try:
+        from thread import get_ident as _get_ident
+    except ImportError:
+        from dummy_thread import get_ident as _get_ident
+    class OrderedDict(dict):
+        'Dictionary that remembers insertion order'
+        # An inherited dict maps keys to values.
+        # The inherited dict provides __getitem__, __len__, __contains__, and get.
+        # The remaining methods are order-aware.
+        # Big-O running times for all methods are the same as regular dictionaries.
+
+        # The internal self.__map dict maps keys to links in a doubly linked list.
+        # The circular doubly linked list starts and ends with a sentinel element.
+        # The sentinel element never gets deleted (this simplifies the algorithm).
+        # Each link is stored as a list of length three:  [PREV, NEXT, KEY].
+
+        def __init__(self, *args, **kwds):
+            '''Initialize an ordered dictionary.  The signature is the same as
+            regular dictionaries, but keyword arguments are not recommended because
+            their insertion order is arbitrary.
+
+            '''
+            if len(args) > 1:
+                raise TypeError('expected at most 1 arguments, got %d' % len(args))
+            try:
+                self.__root
+            except AttributeError:
+                self.__root = root = []                     # sentinel node
+                root[:] = [root, root, None]
+                self.__map = {}
+            self.__update(*args, **kwds)
+
+        def __setitem__(self, key, value, PREV=0, NEXT=1, dict_setitem=dict.__setitem__):
+            'od.__setitem__(i, y) <==> od[i]=y'
+            # Setting a new item creates a new link at the end of the linked list,
+            # and the inherited dictionary is updated with the new key/value pair.
+            if key not in self:
+                root = self.__root
+                last = root[PREV]
+                last[NEXT] = root[PREV] = self.__map[key] = [last, root, key]
+            dict_setitem(self, key, value)
+
+        def __delitem__(self, key, PREV=0, NEXT=1, dict_delitem=dict.__delitem__):
+            'od.__delitem__(y) <==> del od[y]'
+            # Deleting an existing item uses self.__map to find the link which gets
+            # removed by updating the links in the predecessor and successor nodes.
+            dict_delitem(self, key)
+            link_prev, link_next, key = self.__map.pop(key)
+            link_prev[NEXT] = link_next
+            link_next[PREV] = link_prev
+
+        def __iter__(self):
+            'od.__iter__() <==> iter(od)'
+            # Traverse the linked list in order.
+            NEXT, KEY = 1, 2
+            root = self.__root
+            curr = root[NEXT]
+            while curr is not root:
+                yield curr[KEY]
+                curr = curr[NEXT]
+
+        def __reversed__(self):
+            'od.__reversed__() <==> reversed(od)'
+            # Traverse the linked list in reverse order.
+            PREV, KEY = 0, 2
+            root = self.__root
+            curr = root[PREV]
+            while curr is not root:
+                yield curr[KEY]
+                curr = curr[PREV]
+
+        def clear(self):
+            'od.clear() -> None.  Remove all items from od.'
+            for node in self.__map.itervalues():
+                del node[:]
+            root = self.__root
+            root[:] = [root, root, None]
+            self.__map.clear()
+            dict.clear(self)
+
+        # -- the following methods do not depend on the internal structure --
+
+        def keys(self):
+            'od.keys() -> list of keys in od'
+            return list(self)
+
+        def values(self):
+            'od.values() -> list of values in od'
+            return [self[key] for key in self]
+
+        def items(self):
+            'od.items() -> list of (key, value) pairs in od'
+            return [(key, self[key]) for key in self]
+
+        def iterkeys(self):
+            'od.iterkeys() -> an iterator over the keys in od'
+            return iter(self)
+
+        def itervalues(self):
+            'od.itervalues -> an iterator over the values in od'
+            for k in self:
+                yield self[k]
+
+        def iteritems(self):
+            'od.iteritems -> an iterator over the (key, value) pairs in od'
+            for k in self:
+                yield (k, self[k])
+
+        update = MutableMapping.update
+
+        __update = update # let subclasses override update without breaking __init__
+
+        __marker = object()
+
+        def pop(self, key, default=__marker):
+            '''od.pop(k[,d]) -> v, remove specified key and return the corresponding
+            value.  If key is not found, d is returned if given, otherwise KeyError
+            is raised.
+
+            '''
+            if key in self:
+                result = self[key]
+                del self[key]
+                return result
+            if default is self.__marker:
+                raise KeyError(key)
+            return default
+
+        def setdefault(self, key, default=None):
+            'od.setdefault(k[,d]) -> od.get(k,d), also set od[k]=d if k not in od'
+            if key in self:
+                return self[key]
+            self[key] = default
+            return default
+
+        def popitem(self, last=True):
+            '''od.popitem() -> (k, v), return and remove a (key, value) pair.
+            Pairs are returned in LIFO order if last is true or FIFO order if false.
+
+            '''
+            if not self:
+                raise KeyError('dictionary is empty')
+            key = next(reversed(self) if last else iter(self))
+            value = self.pop(key)
+            return key, value
+
+        def __repr__(self, _repr_running={}):
+            'od.__repr__() <==> repr(od)'
+            call_key = id(self), _get_ident()
+            if call_key in _repr_running:
+                return '...'
+            _repr_running[call_key] = 1
+            try:
+                if not self:
+                    return '%s()' % (self.__class__.__name__,)
+                return '%s(%r)' % (self.__class__.__name__, self.items())
+            finally:
+                del _repr_running[call_key]
+
+        def __reduce__(self):
+            'Return state information for pickling'
+            items = [[k, self[k]] for k in self]
+            inst_dict = vars(self).copy()
+            for k in vars(OrderedDict()):
+                inst_dict.pop(k, None)
+            if inst_dict:
+                return (self.__class__, (items,), inst_dict)
+            return self.__class__, (items,)
+
+        def copy(self):
+            'od.copy() -> a shallow copy of od'
+            return self.__class__(self)
+
+        @classmethod
+        def fromkeys(cls, iterable, value=None):
+            '''OD.fromkeys(S[, v]) -> New ordered dictionary with keys from S.
+            If not specified, the value defaults to None.
+
+            '''
+            self = cls()
+            for key in iterable:
+                self[key] = value
+            return self
+
+        def __eq__(self, other):
+            '''od.__eq__(y) <==> od==y.  Comparison to another OD is order-sensitive
+            while comparison to a regular mapping is order-insensitive.
+
+            '''
+            if isinstance(other, OrderedDict):
+                return len(self)==len(other) and self.items() == other.items()
+            return dict.__eq__(self, other)
+
+        def __ne__(self, other):
+            'od.__ne__(y) <==> od!=y'
+            return not self == other
+
+        # -- the following methods support python 3.x style dictionary views --
+
+        def viewkeys(self):
+            "od.viewkeys() -> a set-like object providing a view on od's keys"
+            return KeysView(self)
+
+        def viewvalues(self):
+            "od.viewvalues() -> an object providing a view on od's values"
+            return ValuesView(self)
+
+        def viewitems(self):
+            "od.viewitems() -> a set-like object providing a view on od's items"
+            return ItemsView(self)
+
+
+if __name__ == '__main__':
+    # verify that instances can be pickled
+    from cPickle import loads, dumps
+    Point = namedtuple('Point', 'x, y', True)
+    p = Point(x=10, y=20)
+    assert p == loads(dumps(p, -1))
+
+    # test and demonstrate ability to override methods
+    class Point(namedtuple('Point', 'x y')):
+        @property
+        def hypot(self):
+            return (self.x ** 2 + self.y ** 2) ** 0.5
+        def __str__(self):
+            return 'Point: x=%6.3f y=%6.3f hypot=%6.3f' % (self.x, self.y, self.hypot)
+
+    for p in Point(3,4), Point(14,5), Point(9./7,6):
+        print p
+
+    class Point(namedtuple('Point', 'x y')):
+        'Point class with optimized _make() and _replace() without error-checking'
+        _make = classmethod(tuple.__new__)
+        def _replace(self, _map=map, **kwds):
+            return self._make(_map(kwds.get, ('x', 'y'), self))
+
+    print Point(11, 22)._replace(x=100)
+
+    import doctest
+    TestResults = namedtuple('TestResults', 'failed attempted')
+    print TestResults(*doctest.testmod())
