9b1f53bbe0729c5f2f069c8f0a8de1e374d1e699
riccardo.murri <riccardo.murri@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2012-11-06 15:37:06 +0000

    Ensure sub-tasks of a `SequentialTaskCollection` are attached before performing a `submit()` or `update_state()`.

--- branches/proxy/gc3pie/gc3libs/workflow.py
+++ branches/proxy/gc3pie/gc3libs/workflow.py
@@ -236,9 +236,8 @@ class SequentialTaskCollection(TaskCollection):
         Use the given Controller interface for operations on the job
         associated with this task.
         """
-        for task in self.tasks:
-            if not task._attached:
-                task.attach(controller)
+        if self._current_task is not None:
+            self.tasks[self._current_task].attach(controller)
         Task.attach(self, controller)
 
     def kill(self, **extra_args):
@@ -293,7 +292,7 @@ class SequentialTaskCollection(TaskCollection):
         if self._current_task is None:
             self._current_task = 0
         task = self.tasks[self._current_task]
-        self._controller.submit(task, resubmit, **extra_args)
+        task.attach(self._controller)
         task.submit(resubmit, **extra_args)
         if task.execution.state == Run.State.NEW:
             # submission failed, state unchanged
