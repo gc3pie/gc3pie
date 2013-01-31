3be6903a072bd75e3f8b9ffdfeb0fe52f01ec398
benjamin.jonen <benjamin.jonen@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2013-01-31 16:27:54 +0000

    Test running with python executable `rosenbrock.py` instead of `rosenbrock.cpp`.

--- trunk/gc3pie/gc3libs/optimizer/tests/test_optimizer.py
+++ trunk/gc3pie/gc3libs/optimizer/tests/test_optimizer.py
@@ -27,46 +27,27 @@ import sys
 import logging
 import tempfile
 import shutil
-import logging
-
-import cli.test
 
 from nose.tools import raises, assert_true
 from nose.plugins.skip import SkipTest
+
+import cli.test
 
 import numpy as np
 
 import gc3libs
 from gc3libs import Application
 from gc3libs.cmdline import SessionBasedScript
-from gc3libs.optimizer import update_parameter_in_file
 
 # optimizer specific imports
+from gc3libs.optimizer import draw_population, update_parameter_in_file
 from gc3libs.optimizer.drivers import ParallelDriver
 from gc3libs.optimizer.dif_evolution import DifferentialEvolutionAlgorithm
-from gc3libs.optimizer import draw_population
 
-# Create a temporary stage directory
-temp_stage_dir = tempfile.mkdtemp(prefix = 'ParallelDriver_Rosenbrock_')
-optimization_dir = os.path.join(temp_stage_dir, 'rosenbrock_output_dir')
 
 # Nose will add command line arguments that cannot be interpreted by
 # the SessionBasedScript. To avoid error, override sys.argv.
 sys.argv = ['test_drivers_rosenbrock.py' ]
-
-# General settings
-float_fmt = '%25.15f'
-magic_seed = 100
-pop_size = 5
-itermax = 2
-dim = 2
-lower_bounds = -2 * np.ones(dim)
-upper_bounds = +2 * np.ones(dim)
-
-# Set up logger
-log_file_name = os.path.join(temp_stage_dir, 'DifferentialEvolutionAlgorithm.log')
-log = logging.getLogger("gc3.gc3libs")
-log.setLevel(logging.CRITICAL)
 
 class TestParallelDriver(cli.test.FunctionalTest):
     CONF="""
@@ -88,8 +69,6 @@ type=none
     def __init__(self, *args, **extra_args):
         cli.test.FunctionalTest.__init__(self, *args, **extra_args)
         self.scriptdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../examples/optimizer/rosenbrock')
-        print self.scriptdir
-        pass
 
     def setUp(self):
         cli.test.FunctionalTest.setUp(self)
@@ -110,66 +89,11 @@ type=none
         self.backend = self.core.get_backend('localhost_test')
 
         # -- Rosenbrock setup --
-        # Create base dir
-        temp_base_dir = os.path.join(self.temp_stage_dir, 'base')
-        temp_bin_dir = os.path.join(self.temp_stage_dir, 'bin')
-        os.mkdir(temp_base_dir)
-        para_file = open(os.path.join(temp_base_dir, 'parameters.in'), 'w')
+        # Create base dir with sample parameter file
+        self.temp_base_dir = os.path.join(self.temp_stage_dir, 'base')
+        os.mkdir(self.temp_base_dir)
+        para_file = open(os.path.join(self.temp_base_dir, 'parameters.in'), 'w')
         para_file.write('x1    100.0\nx2    50.0\n')
-        # Create base dir
-        os.mkdir(temp_bin_dir)
-        cpp_file = open(os.path.join(temp_bin_dir, 'rosenbrock.cpp'),'w')
-        cpp_file.write(
-        """
-#include "math.h"
-#include <fstream>
-#include <iostream>
-#include <ostream>
-#include <stdio.h>
-
-
-using std::cout;
-using std::endl;
-
-using std::ifstream;
-using std::ofstream;
-
-int main()
-{
-  cout << "Compute Rosenbrock function" << endl;
-
-  ifstream indata; // indata is like cin
-  double x; // variable for input value
-  double y;
-  std::string a;
-  indata.open("parameters.in"); // opens the file
-  if(!indata) { // file couldn't be opened
-  std::cerr << "Error: file could not be opened" << endl;
-  }
-  indata >> a >> x;
-  indata >> a >> y;
-  indata.close();
-
-  double fun = 100.0 * pow( y - pow(x, 2), 2) + pow( 1 - x, 2);
-  cout << "fun val = " << fun << endl;
-
-  ofstream myfile;
-  myfile.open ("rosenbrock.out");
-  myfile << fun << endl;
-  myfile.close();
-
-  cout << "main ended" << endl;
-}
-        """
-        )
-        cpp_file.flush()
-        # Generate Rosenbrock binary
-        compile_str = 'g++ ' + os.path.join(temp_bin_dir, 'rosenbrock.cpp') + ' -o ' + os.path.join(temp_bin_dir, 'rosenbrock')
-        os.system(compile_str)
-
-        if os.path.isdir(optimization_dir):
-            shutil.rmtree(optimization_dir)
-        os.mkdir(optimization_dir)
 
 
     def cleanup_file(self, fname):
@@ -202,8 +126,8 @@ int main()
                                  '--pop_size', 5,
                                  '--y_conv_crit', 50.0,
                                  '--path_to_stage_dir', self.temp_stage_dir, 
-                                 '--path_to_base_dir', os.path.join(self.scriptdir, 'base'),
-                                 '--path_to_executable', os.path.join(self.scriptdir, 'bin/rosenbrock'),
+                                 '--path_to_base_dir', self.temp_base_dir,
+                                 '--path_to_executable', os.path.join(os.getcwd(), 'rosenbrock.py'),#os.path.join(self.scriptdir, 'bin/rosenbrock'),
                                  '-v')
         
         assert_true(result.stderr.find('Converged:'))
