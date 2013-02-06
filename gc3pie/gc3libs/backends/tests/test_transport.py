3f8d3c37344a469e7162eab1b810be5de1412cb7
antonio.s.messina <antonio.s.messina@gmail.com>
2013-02-06 17:50:46 +0000

    Fix compatibility issue with MacOSX

--- gc3pie/gc3libs/backends/tests/test_transport.py
+++ gc3pie/gc3libs/backends/tests/test_transport.py
@@ -37,7 +37,7 @@ from gc3libs.exceptions import TransportError
 class StubForTestTransport:
 
     def extraSetup(self):
-        (exitcode, tmpdir, stderror) = self.transport.execute_command('mktemp -d')
+        (exitcode, tmpdir, stderror) = self.transport.execute_command('mktemp -d /tmp/test_transport.XXXXXXXXX')
         self.tmpdir = tmpdir.strip()
 
     def tearDown(self):
