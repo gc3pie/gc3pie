6fac6c2816def10e36ebc4096708da3acdbfeb59
antonio.s.messina <antonio.s.messina@gmail.com>
2013-07-11 10:38:06 +0000

    fix TypeError in ec2.py

--- gc3pie/gc3libs/backends/ec2.py
+++ gc3pie/gc3libs/backends/ec2.py
@@ -821,7 +821,7 @@ class EC2Lrms(LRMS):
                     self.name, len(self._vms), self.vm_pool_max_size)
                 raise RecoverableError(
                     "Already running the maximum number of VM on resource %s:"
-                    " %d >= %d." % self.name, len(self._vms), self.vm_pool_max_size)
+                    " %d >= %d." % (self.name, len(self._vms), self.vm_pool_max_size))
 
         # If we reached this point, we are waiting for a VM to be
         # ready, so delay the submission until we wither can submit to
