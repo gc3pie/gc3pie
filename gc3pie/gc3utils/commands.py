54b79031a541dc7edeb12225e6e84bc6b0e579c3
riccardo.murri <riccardo.murri@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2013-07-19 15:40:23 +0000

    gcloud: Clarify help message for option `-n`.

--- gc3pie/gc3utils/commands.py
+++ gc3pie/gc3utils/commands.py
@@ -3,7 +3,7 @@
 """
 Implementation of the `core` command-line front-ends.
 """
-# Copyright (C) 2009-2012 GC3, University of Zurich. All rights reserved.
+# Copyright (C) 2009-2013 GC3, University of Zurich. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -1340,8 +1340,8 @@ To get detailed info on a specific command, run:
             default=None, help="Select resource by name.")
         listparser.add_argument(
             '-n', '--no-update', action="store_false", dest="update",
-            help="Do not update job statuses; only print what's in the "
-            "local database.")
+            help="Do not update job and flavor information;"
+            " only print what is in the local database.")
         terminateparser = self._add_subcmd(
             'terminate',
             self.terminate_vm,
