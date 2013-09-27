97009460bd32b3bbf03e9abd3d7a776fc3b3316e
tyanko.alexiev@gmail.com <tyanko.alexiev@gmail.com@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2013-09-27 07:58:45 +0000

    The following changes have been done:
    * moved the shell script used for the job submission in the VM
    outside the gtsub_control.py script
    * renamed the _scan_and_tar method to _prepare_tar,
    * optimized the _get_output_dir_naming method,
    * removed the after_main_loop method,
    * contents and typo fixes.

--- trunk/gc3pie/gc3apps/geotop/gtsub_control.py
+++ trunk/gc3pie/gc3apps/geotop/gtsub_control.py
@@ -1,21 +1,34 @@
 #! /usr/bin/env python
 #
-# Copyright (C) 2012, 2013, GC3, University of Zurich. All rights reserved.
+#   Copyright (C) 2010 2013 GC3, University of Zurich
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+"""
+Front-end script which runs multiple GEOTop simulations over 
+a presegmented geographical area (usually 18 segments). The 
+workflow is as follows:
+
+- identify the segment in the global map,
+- run GEOTop on the sub-parts of the segment,
+- agregate the results into a set of output files,
+
+It uses the generic `gc3libs.cmdline.SessionBasedScript` framework.
+
+See the output of ``gtsub_control --help`` for program usage instructions.
+
+"""
 __docformat__ = 'reStructuredText'
 __version__ = '$Revision$'
 
@@ -38,109 +51,22 @@ from gc3libs.quantity import Memory, GB
 from gc3libs.cmdline import SessionBasedScript, existing_directory, existing_file
 from gc3libs.workflow import TaskCollection
 from gc3libs.workflow import ParallelTaskCollection, SequentialTaskCollection
+from pkg_resources import Requirement, resource_filename 
 
 GEOTOP_INPUT_ARCHIVE = "input.tgz"
 
-class GTSubControllApplication(Application):
+class GTSubControlApplication(Application):
 
     # This part starts the different Applications for the
     # specified simulations boxes.
 
     def __init__(self, sim_box, inputs, **extra_args):
 
-        # prepare execution script from command
-        execution_script = """
-#!/bin/sh
 
-# check tar
-RES=`command tar --version 2>&1`
-if [ $? -ne 0 ]; then
-     echo "[tar command is not present]"
-     echo $RES
-     exit 1
-else
-     echo "[OK, tar command is present, continue...]"
-fi
-
-# Untar the input files in the
-tar -xzvf input.tgz -C .
-if [ $? -ne 0 ]; then
-     echo "[untar failed]"
-     exit 1
-else
-    echo "[OK, untar of the input archive command finished successfully, continue...]"
-fi
-# Remove input.tgz
-rm input.tgz
-
-# create the sym link to the topo directory
-echo -n "Checking Input folder..."
-if [ -d ./sim/_master/topo ]; then
-    echo "[${PWD}/sim/_master/topo]"
-elif [ -d ${HOME}/sim/_master/topo ]; then
-    ln -s ${HOME}/sim/_master/topo ./sim/_master/topo
-    if [ $? -ne 0 ]; then
-        echo "[creating sim link to the available topo directory has failed]"
-        exit 1
-    else
-        echo "[${HOME}/sim_master/topo]"
-        echo "[OK, sym link to the available topo data has been created, continue...]"
-    fi
-else
-    echo "[FAILED: no folder found in './sim/_master/topo' nor in '${HOME}/sim/_master/topo']"
-    exit 1
-fi
-
-# change the root dir be used
-sed -i -e "s|root=|root='${PWD}/'|g" ./src/TopoAPP/topoApp_complete.r
-if [ $? -ne 0 ]; then
-     echo "[sed the ROOT directory in topoApp_complete.r failed]"
-     exit 1
-else
-    echo "[OK, changing the root directory in the topoApp_complete.r, continue...]"
-fi
-
-# copy parfile if it is present in the root directory
-if [ -f ./parfile.r ]; then
-        echo "[It seems that a different parfile has been passed to the application, replacing the current one...]"
-        cp ./parfile.r ./src/TopoAPP/
-        if [ $? -ne 0 ]; then
-            echo "[Some problems copying the specified parfile.r occured, please check...]"
-            exit 1
-        fi
-else
-    echo "[No parameter file has been specified, using the one passed through the input.tgz file]"
-fi
-
-# change the box sequence to be used
-sed -i -e 's/nboxSeq=/nboxSeq=%s/g' ./src/TopoAPP/parfile.r
-if [ $? -ne 0 ]; then
-     echo "[sed the nboxSeq sequence failed]"
-     exit 1
-else
-    echo "[OK, changing the nboxSeq has been done correctly, continue...]"
-fi
-
-# check R
-RES=`command R --version 2>&1`
-if [ $? -ne 0 ]; then
-     echo "[failed]"
-     echo $RES
-     exit 1
-else
-     echo "[OK, R command is present, starting R script]"
-fi
-R CMD BATCH --no-save --no-restore ./src/TopoAPP/topoApp_complete.r
-        """ % (sim_box)
-
-        # create script file
-        (handle, self.tmp_filename) = tempfile.mkstemp(prefix='gc3pie-gtsub_control', suffix=str(sim_box))
-        fd = open(self.tmp_filename,'w')
-        fd.write(execution_script)
-        fd.close()
-        os.chmod(fd.name,0777)
-
-        inputs[fd.name] = 'gtsub_control.sh'
+        # source the execution script
+        run_gtsub_control = resource_filename(Requirement.parse("gc3pie"), "gc3libs/etc/run_gtsub_control.sh")
+        os.chmod(run_gtsub_control,0777) 
+        inputs[run_gtsub_control] = 'run_gtsub_control.sh'
 
         outputs = {
             './sim/result/':             'sim/result',
@@ -149,7 +75,7 @@ R CMD BATCH --no-save --no-restore ./src/TopoAPP/topoApp_complete.r
         }
 
         Application.__init__(self,
-                arguments = ['./gtsub_control.sh'],
+                arguments = [ "./run_gtsub_control.sh", sim_box ],
                 inputs = inputs,
                 outputs = outputs,
                 stdout = 'gt_sub.log',
@@ -195,7 +121,7 @@ class GTSubControlScript(SessionBasedScript):
             raise gc3libs.exceptions.InvalidUsage(
                 "Invalid argument '%s', use on of the following formats: INT:INT | INT,INT,INT,..,INT | INT " % (nseq,))
 
-    def _scan_and_tar(self, simulation_dir):
+    def _prepare_tar(self, simulation_dir):
         """Prepare the input tarball."""
         try:
             gc3libs.log.debug("Compressing input folder in'%s' ", simulation_dir)
@@ -231,53 +157,64 @@ class GTSubControlScript(SessionBasedScript):
         output_dir_naming_strings = []
 
         # Get the nclust value from the parfile.r
-        for line in gc3libs.utils.fgrep("Nclust=", paramfile):
-            line = line.strip()
-            start, end = line.split('=')
-            if (end == "" or start == ""):
-                raise ValueError("Nclust option in the parfile.r is not properly set."
-                                 " It is set like %s, must be Nclust=Value." % ( line ))
-            nclust = "Nc"+end
-            output_dir_naming_strings.append(nclust)
-            break
+        lines = list(gc3libs.utils.fgrep("Nclust=", paramfile))
+        if len(lines) != 1:
+          raise RuntimeError(
+              "Expecting one and only one `Nclust=` line in %s, found %d"
+              % (paramfile, len(lines)))
+        line = lines[0].strip() 
+        start, end = line.split('=')
+        if (end == "" or start == ""):
+            raise ValueError("Nclust option in the parfile.r is not properly set."
+                             " It is set like %s, must be Nclust=Value." % ( line ))
+        nclust = "Nc"+end
+        output_dir_naming_strings.append(nclust)
 
         # Get the col value from the parfile.r
-        for line in gc3libs.utils.fgrep("col=", paramfile):
-            line = line.strip()
-            start, end = line.split('=')
-            if (end == "" or start == ""):
-                raise ValueError("col option in the parfile.r is not properly set."
-                                " It is set like %s, must be col=Value." % ( line ))
-            col = end
-            col = col.replace('"','')
-            output_dir_naming_strings.append(col)
-            break
+        lines = list(gc3libs.utils.fgrep("col=", paramfile))
+        if len(lines) != 1:
+          raise RuntimeError(
+              "Expecting one and only one `col=` line in %s, found %d"
+              % (paramfile, len(lines)))
+        line = lines[0].strip()
+        if (end == "" or start == ""):
+            raise ValueError("col option in the parfile.r is not properly set."
+                            " It is set like %s, must be col=Value." % ( line ))
+        col = end
+        col = col.replace('"','')
+        output_dir_naming_strings.append(col)
 
         # Get the beginning value out of the "beg" line
-        for i in gc3libs.utils.fgrep("beg <-", paramfile):
-            i = i.strip()
-            line_elements = i.split(' ')
-            beg = line_elements[2].replace('"','')
-            beg = beg.replace('/','')
-            output_dir_naming_strings.append(beg)
-            break
+        lines = list(gc3libs.utils.fgrep("beg <-", paramfile))
+        if len(lines) != 1:
+          raise RuntimeError(
+              "Expecting one and only one `col=` line in %s, found %d"
+              % (paramfile, len(lines)))
+        line = lines[0].strip() 
+        line_elements = line.split(' ')
+        beg = line_elements[2].replace('"','')
+        beg = beg.replace('/','')
+        output_dir_naming_strings.append(beg)
 
         # Get the ending value out of the "end" line
-        for i in gc3libs.utils.fgrep("end <-", paramfile):
-            i = i.strip()
-            line_elements = i.split(' ')
-            end = line_elements[2].replace('"','')
-            end = end.replace('/','')
-            output_dir_naming_strings.append(end)
-            break
+        lines = list(gc3libs.utils.fgrep("end <-", paramfile))
+        if len(lines) != 1:
+          raise RuntimeError(
+              "Expecting one and only one `col=` line in %s, found %d"
+              % (paramfile, len(lines)))
+        line = lines[0].strip()       
+        line_elements = line.split(' ')
+        end = line_elements[2].replace('"','')
+        end = end.replace('/','')
+        output_dir_naming_strings.append(end)
 
         return output_dir_naming_strings
 
     def new_tasks(self, extra):
 
-        # Call the _scan_and_tar method which recreates
+        # Call the _prepare_tar method which recreates
         # the tarball each time.
-        inputs = dict(self._scan_and_tar(self.params.root))
+        inputs = dict(self._prepare_tar(self.params.root))
 
         # Manage the parfile.r and call the _get_output_dir_formatting
         # method which gives back the strings needed for the output dir
@@ -292,21 +229,15 @@ class GTSubControlScript(SessionBasedScript):
         # create tasks for the specified sumulation boxes
         for sim_box in self.sim_boxes:
             extra_args = extra.copy()
-            jobname = 'GTSC' + '_nS' + str(sim_box)+'_'+out_dir_nm[0]+'_'+out_dir_nm[1]+'_'+out_dir_nm[2]+'_'+out_dir_nm[3] ## specifiedcify the jobname by root and number of box seq
+            ## specifiedcify the jobname by root and number of box seq
+            jobname = ('GTSC_nS%s_%s_%s_%s_%s' 
+                        % (sim_box, out_dir_nm[0], out_dir_nm[1], 
+                            out_dir_nm[2], out_dir_nm[3])) 
             extra_args['jobname'] = jobname
-            yield GTSubControllApplication(
+            yield GTSubControlApplication(
                 sim_box, # pass the simulation box number
                 inputs.copy(),
                 **extra_args)
-
-    def after_main_loop(self):
-        """
-        This method prints information about the status of
-        the execution at the end of each sumulation.
-        """
-        for task in self.session:
-            gc3libs.log.info("Task %s: %s terminated with exit code: %s"
-                             % (task.jobname, task.execution.state, task.execution.returncode))
 
 ## main: run tests
 
