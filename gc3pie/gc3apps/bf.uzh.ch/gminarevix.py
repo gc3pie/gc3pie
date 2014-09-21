c259e32e7db58ccb15765792bc75669edf06d002
sergio.maffioletti <sergio.maffioletti@gmail.com>
2014-09-21 21:04:22 +0000

    Sergio: new default specs for Wishart

--- gc3pie/gc3apps/bf.uzh.ch/gminarevix.py
+++ gc3pie/gc3apps/bf.uzh.ch/gminarevix.py
@@ -68,7 +68,7 @@ from gc3libs.quantity import Memory, kB, MB, MiB, GB, Duration, hours, minutes, 
 from gc3libs.workflow import RetryableTask
 
 # Resource requirements depending on Model type (only # cores)
-MODELS_SPECS = {'Wishart': {'requested_cores': 8, 'requested_memory': Memory(16*GB)}, 
+MODELS_SPECS = {'Wishart': {'requested_cores': 8, 'requested_memory': Memory(12*GB)}, 
                 'SVJJ': {'requested_cores': 16, 'requested_memory': Memory(32*GB)}}
 
 ## custom application class
