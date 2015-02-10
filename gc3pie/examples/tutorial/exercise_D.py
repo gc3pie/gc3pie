d52626d2f8b853a61b1a019ba42881448d9ab035
riccardo.murri <riccardo.murri@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2015-02-10 14:44:58 +0000

    Update remaining copyright notices.
    Group name has changed to "Service and Support for Science IT" and is
    now part of "Zentrale Informatik" of the University of Zurich.  Update
    the copyright banner accordingly.

--- gc3pie/examples/tutorial/exercise_D.py
+++ gc3pie/examples/tutorial/exercise_D.py
@@ -12,7 +12,7 @@ and runs one job per each file in directory that matches the given
 pattern
 
 """
-# Copyright (C) 2012, GC3, University of Zurich. All rights reserved.
+# Copyright (C) 2012 S3IT, Zentrale Informatik, University of Zurich. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
--- gc3pie/examples/tutorial/exercise_F.py
+++ gc3pie/examples/tutorial/exercise_F.py
@@ -13,7 +13,7 @@ Note: instead of using a shell script, we use the bash command::
 
 which will exit with an exit code between 1 and 6.
 """
-# Copyright (C) 2012, GC3, University of Zurich. All rights reserved.
+# Copyright (C) 2012 S3IT, Zentrale Informatik, University of Zurich. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
