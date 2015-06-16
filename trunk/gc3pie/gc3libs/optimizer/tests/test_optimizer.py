45c8279968860eb74df127d1149acb08fd35457e
riccardo.murri <riccardo.murri@d0f2d5a0-84c0-18d2-3862-8add1af26540>
2015-06-16 22:50:21 +0000

    Update copyright notice.
    Do not mention GC3, attribute copyright to the "University of Zurich"
    which is the legal entity -- the rest is information about authorship
    which has a different place.

--- trunk/gc3pie/gc3libs/optimizer/tests/test_optimizer.py
+++ trunk/gc3pie/gc3libs/optimizer/tests/test_optimizer.py
@@ -3,7 +3,7 @@
 """
 Unit tests for the `gc3libs.optimizer.drivers` module.
 """
-# Copyright (C) 2011, 2012, 2013, GC3, University of Zurich. All rights reserved.
+# Copyright (C) 2011, 2012, 2013, University of Zurich. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
--- trunk/gc3pie/gc3libs/quantity.py
+++ trunk/gc3pie/gc3libs/quantity.py
@@ -8,7 +8,7 @@ For details and the discussion leading up to this,
 see: `<http://code.google.com/p/gc3pie/issues/detail?id=47>`
 
 """
-# Copyright (C) 2011 - 2014, GC3, University of Zurich. All rights reserved.
+# Copyright (C) 2011 - 2014, University of Zurich. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
