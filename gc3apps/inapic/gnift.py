14b06d06708fb3c7b0bc8554371384ed6607bfb8
Sergio Maffioletti <sergio.maffioletti@uzh.ch>
2016-01-28 16:27:11 +0100

    Sergio: first working version of gnift for INAPIC support.

---
+++ gc3apps/inapic/gnift.py
@@ -0,0 +1,185 @@
+#! /usr/bin/env python
+#
+#   gnift.py -- Front-end script for running the docking program rDock
+#   over a list of ligand files.
+#
+#   Copyright (C) 2014, 2015 S3IT, University of Zurich
+#
+#   This program is free software: you can redistribute it and/or
+#   modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+
+It uses the generic `gc3libs.cmdline.SessionBasedScript` framework.
+
+See the output of ``gnift.py --help`` for program usage
+instructions.
+
+Input parameters consists of:
+
+...
+
+Options:
+"""
+
+__version__ = 'development version (SVN $Revision$)'
+# summary of user-visible changes
+__changelog__ = """
+  2015-02-17:
+  * Initial version
+"""
+__author__ = 'Sergio Maffioletti <sergio.maffioletti@uzh.ch>'
+__docformat__ = 'reStructuredText'
+
+
+# run script, but allow GC3Pie persistence module to access classes defined here;
+# for details, see: https://github.com/uzh/gc3pie/issues/95
+if __name__ == "__main__":
+    import gnift
+    gnift.GniftScript().run()
+
+import os
+import sys
+import time
+import tempfile
+import re
+
+import shutil
+# import csv
+
+from pkg_resources import Requirement, resource_filename
+
+import gc3libs
+import gc3libs.exceptions
+from gc3libs import Application, Run, Task
+from gc3libs.cmdline import SessionBasedScript, executable_file
+import gc3libs.utils
+from gc3libs.quantity import Memory, kB, MB, MiB, GB, Duration, hours, minutes, seconds
+from gc3libs.workflow import RetryableTask
+
+DEFAULT_CORES = 1
+DEFAULT_MEMORY = Memory(3000,MB)
+
+DEFAULT_REMOTE_INPUT_FOLDER="./input/"
+DEFAULT_REMOTE_OUTPUT_FOLDER="./output"
+
+## custom application class
+class GniftApplication(Application):
+    """
+    """
+    application_name = 'gnifti'
+    
+    def __init__(self, subject, subject_folder, **extra_args):
+
+        self.output_dir = extra_args['output_dir']
+
+        inputs = dict()
+        outputs = dict()
+
+        gnift_wrapper_sh = resource_filename(Requirement.parse("gc3pie"),
+                                              "gc3libs/etc/gnift_wrapper.py")
+        inputs[gnift_wrapper_sh] = os.path.basename(gnift_wrapper_sh)
+        inputs[subject_folder] = DEFAULT_REMOTE_INPUT_FOLDER
+
+        arguments = "./%s %s %s %s" % (inputs[gnift_wrapper_sh],
+                                    subject,
+                                    DEFAULT_REMOTE_INPUT_FOLDER,
+                                    DEFAULT_REMOTE_OUTPUT_FOLDER)
+
+        Application.__init__(
+            self,
+            arguments = arguments,
+            inputs = inputs,
+            outputs = [DEFAULT_REMOTE_OUTPUT_FOLDER],
+            stdout = 'gnift.log',
+            join=True,
+            **extra_args)        
+
+class GniftScript(SessionBasedScript):
+    """
+    
+    The ``gnift`` command keeps a record of jobs (submitted, executed
+    and pending) in a session file (set name with the ``-s`` option); at
+    each invocation of the command, the status of all recorded jobs is
+    updated, output from finished jobs is collected, and a summary table
+    of all known jobs is printed.
+    
+    Options can specify a maximum number of jobs that should be in
+    'SUBMITTED' or 'RUNNING' state; ``gnift`` will delay submission of
+    newly-created jobs so that this limit is never exceeded.
+    """
+
+    def __init__(self):
+        SessionBasedScript.__init__(
+            self,
+            version = __version__, # module version == script version
+            application = GniftApplication, 
+            # only display stats for the top-level policy objects
+            # (which correspond to the processed files) omit counting
+            # actual applications because their number varies over
+            # time as checkpointing and re-submission takes place.
+            stats_only_for = GniftApplication,
+            )
+ 
+    def setup_args(self):
+
+        self.add_param('input_data', type=str,
+                       help="Root localtion of input data. "
+                       "Note: expected folder structure: "
+                       " 1 subfodler for each subject. "
+                       " In each subject folder, " 
+                       " 1 subfolder for each TimePoint. "
+                       " Each TimePoint folder should contain 2 input "
+                       "NFTI files.")
+
+    def new_tasks(self, extra):
+        """
+        For each input folder, create an instance of GniftApplication
+        """
+        tasks = []
+
+        for subject_folder in self.get_input_subject_folder(self.params.input_data):
+        
+            # extract root folder name to be used as jobname
+            subjectname = subject_folder
+
+            extra_args = extra.copy()
+            extra_args['jobname'] = subjectname
+
+            extra_args['output_dir'] = self.params.output
+            extra_args['output_dir'] = extra_args['output_dir'].replace('NAME', 
+                                                                        'run_%s' % subjectname)
+            extra_args['output_dir'] = extra_args['output_dir'].replace('SESSION', 
+                                                                        'run_%s' % subjectname)
+            extra_args['output_dir'] = extra_args['output_dir'].replace('DATE', 
+                                                                        'run_%s' % subjectname)
+            extra_args['output_dir'] = extra_args['output_dir'].replace('TIME', 
+                                                                        'run_%s' % subjectname)
+            
+            tasks.append(GniftApplication(
+                subjectname,
+                os.path.join(self.params.input_data,subject_folder),
+                **extra_args))
+            
+        return tasks
+
+    def get_input_subject_folder(self, input_folder):
+        """
+        Check and validate input subfolders
+        XXX: for the time being just pass
+        """
+        return os.listdir(input_folder)
+        # for folder in os.listdir(input_folder):
+        #     yield os.path.abspath(os.path.join(input_folder,folder))
+            
---
+++ gc3libs/etc/gnift_wrapper.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+"""
+export SUBJECTS_DIR=/our/out/data/path
+
+************
+SUBJECT: s01
+************
+ ************
+ DATA INPUT s01
+ ************
+recon-all -i in_data_path/s01/TP1/T1w_a.nii.gz -i in_data_path/s01/TP1/T1w_b.nii.gz -subjid s01.cross.TP1
+recon-all -i in_data_path/s01/TP2/T1w_a.nii.gz -i in_data_path/s01/TP2/T1w_b.nii.gz -subjid s01.cross.TP2
+recon-all -i in_data_path/s01/TP3/T1w_a.nii.gz -i in_data_path/s01/TP3/T1w_b.nii.gz -subjid s01.cross.TP3
+
+ ************
+ CROSS SECTIONAL PROCESSING s01
+ ************
+recon-all -s s01.cross.TP1 -all
+recon-all -s s01.cross.TP2 -all
+recon-all -s s01.cross.TP3 -all
+
+ ************
+ BASE PROCESSING s01
+ ************
+recon-all -base s01.base -tp s01.cross.TP1 -tp s01.cross.TP2 -tp s01.cross.TP3 -all
+
+ ************
+ LONG PROCESSING s01
+ ************
+recon-all -long s01.cross.TP1 s01.base -all -qcache
+recon-all -long s01.cross.TP2 s01.base -all -qcache
+recon-all -long s01.cross.TP3 s01.base -all -qcache
+"""
+
+import sys
+import os
+import subprocess
+
+NII_EXTENSION = "nii"
+FSAVERAGE = "fsaverage"
+FS_SUBJECT_FSAVERAGE = os.path.join(os.environ["FREESURFER_HOME"],"subjects",FSAVERAGE)
+
+def Usage():
+    print ("Usage: gnift_wrapper.py <subject name> <NIFIT input folder> <subjects dir>")
+
+def RunFreesurfer(subject, input, output):
+    """
+    Walk through `input` and search for every subfolder with at least 1 `nii` file.
+    Record reference to `nii` files per subfolder.
+    Run Data Input, Cross Sectional, Base and Long for all of them.
+    Use only 1 core, run the step sequentially.
+    """
+
+    # Create output folder and add simlink to FSAVERAGE
+    os.mkdir(output)
+    os.symlink(FS_SUBJECT_FSAVERAGE,os.path.join(output,FSAVERAGE))
+    
+    input_nii = dict()
+    cross_files = []
+    os.environ["SUBJECTS_DIR"] = output
+
+    for r,d,f in os.walk(input):
+        for filename in f:
+            if filename.endswith(NII_EXTENSION):
+                timepoint = os.path.basename(r)
+                if not timepoint in input_nii.keys():
+                    # Initialise
+                    input_nii[timepoint] = list()
+                input_nii[timepoint].append(os.path.join(r,filename))
+
+
+    # DATA INPUT and  CROSS SECTIONAL PROCESSING
+    print "Start DATA INPUT on %d Timepoints" % len(input_nii.keys())
+    for timepoint in input_nii.keys():
+        inputs = '-i '+' -i '.join(x for x in input_nii[timepoint])
+        data_input_outputfolder =  "%s.cross.%s" % (subject, timepoint)
+        
+        "Example: recon-all -i in_data_path/s01/TP1/T1w_a.nii.gz -i in_data_path/s01/TP1/T1w_b.nii.gz -subjid s01.cross.TP1"
+        command="recon-all %s -subjid %s" % (inputs, data_input_outputfolder)
+        runme(command)
+
+        # Verify output
+        try:
+            assert os.path.isdir(os.path.join(output,data_input_outputfolder)), \
+            "Output folder '%s' not found" % os.path.join(output,data_input_outputfolder)
+        except AssertionError as ex:
+            print ex.message
+            return 1
+            
+        cross_files.append(data_input_outputfolder)
+        
+        print "Start CROSS SECTIONAL PROCESSING"
+        "Example: recon-all -s s01.cross.TP1 -all"
+        command="recon-all -s %s -all" % data_input_outputfolder
+        runme(command)
+
+
+    # BASE PROCESSING    
+    print "Start BASE PROCESSING with %d timepoints" % len(cross_files)
+    inputs = '-tp '+' -tp '.join(x for x in cross_files)
+    basefile = "%s.base" % subject
+    command = "recon-all -base %s %s -all" % (basefile,inputs)
+    runme(command)
+    # XXX: what to verify here ?
+
+
+    # LONG PROCESSING s01
+    print "Start LONG PROCESSING with %d timepoints" % len(cross_files)
+    for cross in cross_files:
+        # Example: recon-all -long s01.cross.TP1 s01.base -all -qcache
+        command = "recon-all -long %s %s -all -qcache" % (cross,basefile)
+        runme(command)
+        # XXX: what to verify here ?
+
+def runme(command):
+    """
+    Comodity function to run commands using `subprocess` module
+    Input: command to run
+    Output: none
+    Raise Exception in case command fails
+    """
+    proc = subprocess.Popen(
+        [command],
+        shell=True,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE)
+    
+    print "Running command %s" % command
+    (stdout, stderr) = proc.communicate()
+    
+    if proc.returncode != 0:
+        print "Execution failed with exit code: %d" % proc.returncode
+        print "Output message: %s" % stdout
+        print "Error message: %s" % stderr
+        raise Exeption(stderr)
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 4):
+        sys.exit(Usage())
+    sys.exit(RunFreesurfer(sys.argv[1], sys.argv[2], sys.argv[3]))
