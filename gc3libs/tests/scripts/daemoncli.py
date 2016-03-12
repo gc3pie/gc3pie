7105b0a2dc2b11191621c3f3f962776e18e63cc0
Antonio Messina <antonio.s.messina@gmail.com>
2016-03-12 17:00:00 +0100

    Implement simple IPC mechanism to interact with SessionBasedDaemon

---
+++ gc3libs/tests/scripts/daemoncli.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+#
+"""
+"""
+# Copyright (C) 2012-2013, GC3, University of Zurich. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+__docformat__ = 'reStructuredText'
+__version__ = '$Revision$'
+
+
+import oi
+import sys
+import os
+
+if len(sys.argv) < 2:
+    print("Usage: %s <path-to-socket>" % sys.argv[0])
+    sys.exit(1)
+
+path=os.path.abspath(sys.argv[1])
+del sys.argv[1]
+# Seems that the client program also get commands via arguments
+ctl = oi.CtlProgram('cli', address='ipc://%s' % path)
+ctl.run()
