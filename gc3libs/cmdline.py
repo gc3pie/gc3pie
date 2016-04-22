dae9d3d6b6a0ccdf8a84bc5c4e5b839e63e86402
Antonio Messina <antonio.s.messina@gmail.com>
2016-04-22 09:38:42 +0200

    Avoid duplicated code.
    Move code shared between `pre_run` methods of `_SessionBasedCommand`
    and `SessionBasedDaemon` to `_SessionBasedCommand._prerun_common_checks()`

--- gc3libs/cmdline.py
+++ gc3libs/cmdline.py
@@ -1149,21 +1149,7 @@ class _SessionBasedCommand(_Script):
             " session directory, with a '.out' appended.")
         return
 
-    def pre_run(self):
-        """
-        Perform parsing of standard command-line options and call into
-        `parse_args()` to do non-optional argument processing.
-        """
-        # call base classes first (note: calls `parse_args()`)
-        _Script.pre_run(self)
-        # since it may time quite some time before jobs are created
-        # and the first report is displayed, print a startup banner so
-        # that users get some kind of feedback ...
-        print("Starting %s;"
-              " use the '-v' command-line option to get"
-              " a more verbose report of activity."
-              % (self.name,))
-
+    def _prerun_common_checks(self):
         # consistency checks
         try:
             # FIXME: backwards-compatibility, remove after 2.0 release
@@ -1193,6 +1179,24 @@ class _SessionBasedCommand(_Script):
         if ('NAME' not in self.params.output
                 and 'ITER' not in self.params.output):
             self.params.output = os.path.join(self.params.output, 'NAME')
+    
+    def pre_run(self):
+        """
+        Perform parsing of standard command-line options and call into
+        `parse_args()` to do non-optional argument processing.
+        """
+        # call base classes first (note: calls `parse_args()`)
+        _Script.pre_run(self)
+
+        self._prerun_common_checks()
+        # since it may time quite some time before jobs are created
+        # and the first report is displayed, print a startup banner so
+        # that users get some kind of feedback ...
+        print("Starting %s;"
+              " use the '-v' command-line option to get"
+              " a more verbose report of activity."
+              % (self.name,))
+
 
     ##
     # INTERNAL METHODS
@@ -1579,58 +1583,19 @@ class SessionBasedDaemon(_SessionBasedCommand):
 
             self.main = self._main_client
             return
-        # since it may time quite some time before jobs are created
-        # and the first report is displayed, print a startup banner so
-        # that users get some kind of feedback ...
-        print("Starting %s;"
-              " use the '-v' command-line option to get"
-              " a more verbose report of activity."
-              % (self.name,))
 
-        # consistency checks
-        try:
-            # FIXME: backwards-compatibility, remove after 2.0 release
-            self.params.walltime = Duration(int(self.params.wctime), hours)
-        except ValueError:
-            # cannot convert to `int`, use extended parsing
-            self.params.walltime = Duration(self.params.wctime)
-
-        # determine the session file name (and possibly create an empty index)
-
-        self.params.working_dir = os.path.abspath(self.params.working_dir)
         # Default session dir is inside the working directory
         if not self.params.session:
             self.params.session = os.path.join(
                 self.params.working_dir, self.name)
+
+        # Default output directory is the working directory.
         if not self.params.output:
             self.params.output = self.params.working_dir
 
-        self.session_uri = gc3libs.url.Url(self.params.session)
-        if self.params.store_url == 'sqlite':
-            self.params.store_url = (
-                "sqlite:///%s/jobs.db" % self.session_uri.path)
-        elif self.params.store_url == 'file':
-            self.params.store_url = ("file:///%s/jobs" % self.session_uri.path)
-        self.session = self._make_session(
-            self.session_uri.path, self.params.store_url)
-
-        # keep a copy of the credentials in the session dir
-        self.config.auth_factory.add_params(
-            private_copy_directory=self.session.path)
-
-        # XXX: ARClib errors out if the download directory already exists, so
-        # we need to make sure that each job downloads results in a new one.
-        # The easiest way to do so is to append 'NAME' to the `output_dir`
-        # (if it's not already there).
-        if ('NAME' not in self.params.output
-                and 'ITER' not in self.params.output):
-            self.params.output = os.path.join(self.params.output, 'NAME')
-
-        # Create the working directory if it doesn't exsist
-        if not os.path.isdir(self.params.working_dir):
-            self.log.debug("Create working directory %s",
-                           self.params.working_dir)
-            os.mkdir(self.params.working_dir)
+        self._prerun_common_checks()
+        
+        self.params.working_dir = os.path.abspath(self.params.working_dir)
 
         self.params.inbox = [os.path.abspath(p) for p in self.params.inbox]
 
