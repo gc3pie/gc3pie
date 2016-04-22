5b7768c269ffc3fac9f8273512676892494e1bf8
Antonio Messina <antonio.s.messina@gmail.com>
2016-04-22 09:34:39 +0200

    Specify inotifyx event mask plus cosmetic changes

--- gc3apps/irm.uzh.ch/gbeastdaemon.py
+++ gc3apps/irm.uzh.ch/gbeastdaemon.py
@@ -27,6 +27,8 @@ from gc3libs.cmdline import SessionBasedDaemon
 import os
 from gc3libs.quantity import GiB, MiB
 
+import inotifyx
+
 class GBeastApp(gc3libs.Application):
     def __init__(self, beast, jarfile, path, ncores, **extra):
         self.fname = os.path.basename(path)
@@ -53,20 +55,19 @@ class GBeastApp(gc3libs.Application):
             **extra)
 
 class GBeastDaemon(SessionBasedDaemon):
-    """This script gets a directory as only argument. For each file in the
-    directory will run a GBeastApp application.
+    """This daemon script will continuosly check one or more `inbox`
+    directories and, for each *new* XML file created in it, will create
+    a new job running BEAST on it.
 
-    It also check, through inotify, if new files are added to the
-    directory, in that case, a new application is added to the
-    session.
+    By default it will run BEAST v1.8, but if the filename contains
+    the string 'BEAST2' it will use BEAST v2 instead. Path to the
+    correct Beast jar can be specified throught option `--beast1` and
+    `--beast2`.
 
     """
     version = '0.1'
 
     def setup_options(self):
-        """Script to parallelize execution of BEAST over multiple input
-        files. Takes a directory containing XML files, parse"""
-
         self.add_param('--beast1', default='/apps/BEASTv1.8.2/lib/beast.jar', help='Path to BEAST v1 jar file')
         self.add_param('--beast2', default='/apps/BEASTv2.3.2/lib/beast.jar', help='Path to BEAST v2 jar file')
         self.add_param('--cores', default=1, type=int, help="Amount of cores to use. Default: %(default)s")
@@ -77,15 +78,19 @@ class GBeastDaemon(SessionBasedDaemon):
         # here we only need to ensure the fake always-running task is
         # created.
 
-        if not epath or not epath.endswith('.xml'):
-            gc3libs.log.info("Ignoring file %s since it doesn't end in .xml" % epath)
+        if not epath:
+            # The script has just started, don't do anything yet, as
+            # we will always wait for inotify for new files.
+        elif epath.endswith('.xml'):
+            gc3libs.log.info("Ignoring file `%s` since it doesn't end in .xml" % epath)
             return []
 
         beast = 'beast1'
         if 'BEAST2' in epath:
             beast = 'beast2'
         else:
-            gc3libs.log.warning("Unable to guess which version of BEAST you want to run for file %s. Assuming BEAST v1" % epath)
+            gc3libs.log.warning("Unable to guess which version of BEAST you"
+                                " want to run for file %s. Assuming BEAST v1" % epath)
 
         jarfile = self.params.beast1 if beast == 'beast1' else self.params.beast2
         try:
@@ -93,22 +98,23 @@ class GBeastDaemon(SessionBasedDaemon):
             # arguments, otherwise we don't know where to get
             # them.
 
-            # FIXME: What should we do when a file is *updated*?
-            # If you touch an existing file, this will trigger
-            # the creation of a new instance. You could check if
-            # fname is not in
-            #
-            #    [app.path for app in self.session if 'path' in app]
-            #
-            # but it might not be what you want. Maybe a command
-            # line option?
+            # Check if the file is being processed already
+            if epath in [app.path for app in self.session if 'path' in app]:
+                gc3libs.log.warning(
+                    "Ignoring file `%s`, as it's been processed already."
+                    " You can re-process it by renaming the file or using"
+                    " `gresub`" % epath)
+                return []
 
-            app = GBeastApp(beast,
-                            jarfile,
-                            epath,
-                            self.params.cores,
-                            **extra.copy())
-            gc3libs.log.info("Added new application to session for file %s", epath)
+            if emask == inotifyx.IN_CLOSE_WRITE:
+                app = GBeastApp(beast,
+                                jarfile,
+                                epath,
+                                self.params.cores,
+                                **extra.copy())
+                gc3libs.log.info("Added new application to session for file %s", epath)
+            else:
+                gc3libs.log.debug("Ignoring notify mask `%s` for file `%s`" % (emask, epath))
             return [app]
         except Exception as ex:
             gc3libs.log.error("Error while adding application for file %s: %s", epath, ex)
