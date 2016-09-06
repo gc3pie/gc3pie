gc3pie/gc3pie

commit a1e789311006d8d3f2cefb60a4c59bce779e2bd1

New hook `Store._update_to_latest_schema()` to update objects to reflect core schema changes.

gc3libs/persistence/store.py
@@ -85,6 +85,15 @@ class Store(object):
             "Abstract method 'Store.load' called"
             " -- should have been implemented in a derived class!")
 
+    def _update_to_latest_schema(self):
+        """
+        Modify an object in-place to reflect changes in the schema.
+
+        Called immediately after a successful `load()`, just before returning
+        the retrieved object to the caller.
+        """
+        pass
+
     def save(self, obj):
         """
         Save an object, and return an ID.

gc3libs/persistence/filesystem.py
@@ -141,6 +141,8 @@ class FilesystemStore(Store):
                               type(obj.persistent_id),
                               id_,
                               type(id_)))
+
+        super(FilesystemStore, self)._update_to_latest_schema()
         return obj
 
     @same_docstring_as(Store.remove)

gc3libs/persistence/sql.py
@@ -263,6 +263,7 @@ class SqlStore(Store):
         obj = unpickler.load()
         conn.close()
 
+        super(SqlStore, self)._update_to_latest_schema()
         return obj
 
     @same_docstring_as(Store.remove)

commit 3516d378392aad08795469862955394e681820ae

Update `*_instance_id` attributes to `_lrms_vm_id` on load.
This fixes commit b43ad26 so that existing sessions and tasks are not
broken by the commit but are transparently converted (using the hook
introduced in commit a1e7893).

The new name is consistent with the naming convention adopted so far,
that object attributes that are only needed for backends' internal use
begin with a `_lrms_` prefix.

gc3libs/backends/ec2.py
@@ -148,7 +148,7 @@ class EC2Lrms(LRMS):
 
         self.region = ec2_region
 
-        # Mapping of job.execution.ec2_instance_id => LRMS
+        # Mapping of job.execution._lrms_vm_id => LRMS
         self.subresources = {}
 
         auth = self._auth_fn()
@@ -647,7 +647,7 @@ class EC2Lrms(LRMS):
 
     @same_docstring_as(LRMS.cancel_job)
     def cancel_job(self, app):
-        resource = self._get_subresource(self._get_vm(app.execution.ec2_instance_id))
+        resource = self._get_subresource(self._get_vm(app.execution._lrms_vm_id))
         return resource.cancel_job(app)
 
     @same_docstring_as(LRMS.get_resource_status)
@@ -766,22 +766,22 @@ class EC2Lrms(LRMS):
     @same_docstring_as(LRMS.get_results)
     def get_results(self, app, download_dir, overwrite=False,
                     changed_only=True):
-        subresource = self._get_subresource(self._get_vm(app.execution.ec2_instance_id))
+        subresource = self._get_subresource(self._get_vm(app.execution._lrms_vm_id))
         return subresource.get_results(app, download_dir,
                                        overwrite=overwrite,
                                        changed_only=changed_only)
 
     @same_docstring_as(LRMS.update_job_state)
     def update_job_state(self, app):
-        if app.execution.ec2_instance_id not in self.subresources:
+        if app.execution._lrms_vm_id not in self.subresources:
             try:
-                self.subresources[app.execution.ec2_instance_id] = self._get_subresource(
-                    self._get_vm(app.execution.ec2_instance_id))
+                self.subresources[app.execution._lrms_vm_id] = self._get_subresource(
+                    self._get_vm(app.execution._lrms_vm_id))
             except InstanceNotFound as ex:
                 gc3libs.log.error(
                     "Changing state of task '%s' to TERMINATED since EC2 "
                     "instance '%s' does not exist anymore.",
-                    app.execution.lrms_jobid, app.execution.ec2_instance_id)
+                    app.execution.lrms_jobid, app.execution._lrms_vm_id)
                 app.execution.state = Run.State.TERMINATED
                 raise ex
             except UnrecoverableError as ex:
@@ -791,7 +791,7 @@ class EC2Lrms(LRMS):
                 app.execution.state = Run.State.UNKNOWN
                 raise ex
 
-        return self.subresources[app.execution.ec2_instance_id].update_job_state(app)
+        return self.subresources[app.execution._lrms_vm_id].update_job_state(app)
 
     def submit_job(self, job):
         """
@@ -868,7 +868,7 @@ class EC2Lrms(LRMS):
                         or vm.instance_type != instance_type):
                     continue
                 resource.submit_job(job)
-                job.execution.ec2_instance_id = vm_id
+                job.execution._lrms_vm_id = vm_id
                 job.changed = True
                 gc3libs.log.info(
                     "Job successfully submitted to remote resource %s.",
@@ -930,7 +930,7 @@ class EC2Lrms(LRMS):
     @same_docstring_as(LRMS.peek)
     def peek(self, app, remote_filename, local_file, offset=0, size=None):
         resource = self._get_subresource(
-            self._get_vm(app.execution.ec2_instance_id))
+            self._get_vm(app.execution._lrms_vm_id))
         return resource.peek(app, remote_filename, local_file, offset, size)
 
     def validate_data(self, data_file_list=None):
@@ -960,7 +960,7 @@ class EC2Lrms(LRMS):
 
         # freeing the resource from the application is now needed as
         # the same instanc may run multiple applications
-        resource = self._get_subresource(self._get_vm(app.execution.ec2_instance_id))
+        resource = self._get_subresource(self._get_vm(app.execution._lrms_vm_id))
         resource.free(app)
 
         # FIXME: current approach in terminating running instances:
@@ -969,7 +969,7 @@ class EC2Lrms(LRMS):
         resource.get_resource_status()
         if len(resource.job_infos) == 0:
             # turn VM off
-            vm = self._get_vm(app.execution.ec2_instance_id)
+            vm = self._get_vm(app.execution._lrms_vm_id)
             gc3libs.log.info("VM instance %s at %s is no longer needed."
                              " Terminating.", vm.id, vm.public_dns_name)
             del self.subresources[vm.id]

gc3libs/backends/openstack.py
@@ -670,7 +670,7 @@ class OpenStackLrms(LRMS):
     def cancel_job(self, app):
         try:
             subresource = self._get_subresource(
-                self._get_vm(app.execution.os_instance_id))
+                self._get_vm(app.execution._lrms_vm_id))
             return subresource.cancel_job(app)
         except InstanceNotFound:
             # ignore -- if this VM exists no more, we need not cancel any job
@@ -805,18 +805,18 @@ class OpenStackLrms(LRMS):
                     changed_only=True):
         try:
             subresource = self._get_subresource(
-                self._get_vm(app.execution.os_instance_id))
+                self._get_vm(app.execution._lrms_vm_id))
         except InstanceNotFound:
             gc3libs.log.error(
                 "Changing state of task '%s' to TERMINATED since OpenStack"
                 " instance '%s' does not exist anymore.",
-                app.execution.lrms_jobid, app.execution.os_instance_id)
+                app.execution.lrms_jobid, app.execution._lrms_vm_id)
             app.execution.state = Run.State.TERMINATED
             app.execution.signal = Run.Signals.RemoteError
             app.execution.history.append(
                 "State changed to TERMINATED since OpenStack"
                 " instance '%s' does not exist anymore."
-                % (app.execution.os_instance_id,))
+                % (app.execution._lrms_vm_id,))
             raise UnrecoverableDataStagingError(
                 "VM where job was running is no longer available")
         except UnrecoverableError as err:
@@ -836,21 +836,21 @@ class OpenStackLrms(LRMS):
     @same_docstring_as(LRMS.update_job_state)
     def update_job_state(self, app):
         self._connect()
-        if app.execution.os_instance_id not in self.subresources:
+        if app.execution._lrms_vm_id not in self.subresources:
             try:
-                self.subresources[app.execution.os_instance_id] = self._get_subresource(
-                    self._get_vm(app.execution.os_instance_id))
+                self.subresources[app.execution._lrms_vm_id] = self._get_subresource(
+                    self._get_vm(app.execution._lrms_vm_id))
             except InstanceNotFound:
                 gc3libs.log.error(
                     "Changing state of task '%s' to TERMINATED since OpenStack"
                     " instance '%s' does not exist anymore.",
-                    app.execution.lrms_jobid, app.execution.os_instance_id)
+                    app.execution.lrms_jobid, app.execution._lrms_vm_id)
                 app.execution.state = Run.State.TERMINATED
                 app.execution.signal = Run.Signals.RemoteError
                 app.execution.history.append(
                     "State changed to TERMINATED since OpenStack"
                     " instance '%s' does not exist anymore."
-                    % (app.execution.os_instance_id,))
+                    % (app.execution._lrms_vm_id,))
                 raise
             except UnrecoverableError as err:
                 gc3libs.log.error(
@@ -863,7 +863,7 @@ class OpenStackLrms(LRMS):
                     " an OpenStack API error (%s: %s)."
                     % (err.__class__.__name__, err))
                 raise
-        return self.subresources[app.execution.os_instance_id].update_job_state(app)
+        return self.subresources[app.execution._lrms_vm_id].update_job_state(app)
 
     def submit_job(self, job):
         """
@@ -932,7 +932,7 @@ class OpenStackLrms(LRMS):
                 if vm.image['id'] != image_id:
                     continue
                 subresource.submit_job(job)
-                job.execution.os_instance_id = vm_id
+                job.execution._lrms_vm_id = vm_id
                 job.changed = True
                 gc3libs.log.info(
                     "Job successfully submitted to remote resource %s.",
@@ -997,18 +997,18 @@ class OpenStackLrms(LRMS):
     def peek(self, app, remote_filename, local_file, offset=0, size=None):
         try:
             subresource = self._get_subresource(
-                self._get_vm(app.execution.os_instance_id))
+                self._get_vm(app.execution._lrms_vm_id))
         except InstanceNotFound:
             gc3libs.log.error(
                 "Changing state of task '%s' to TERMINATED since OpenStack"
                 " instance '%s' does not exist anymore.",
-                app.execution.lrms_jobid, app.execution.os_instance_id)
+                app.execution.lrms_jobid, app.execution._lrms_vm_id)
             app.execution.state = Run.State.TERMINATED
             app.execution.signal = Run.Signals.RemoteError
             app.execution.history.append(
                 "State changed to TERMINATED since OpenStack"
                 " instance '%s' does not exist anymore."
-                % (app.execution.os_instance_id,))
+                % (app.execution._lrms_vm_id,))
             raise UnrecoverableDataStagingError(
                 "VM where job was running is no longer available.")
         return subresource.peek(app, remote_filename, local_file, offset, size)
@@ -1042,7 +1042,7 @@ class OpenStackLrms(LRMS):
         # the same instanc may run multiple applications
         try:
             subresource = self._get_subresource(
-                self._get_vm(app.execution.os_instance_id))
+                self._get_vm(app.execution._lrms_vm_id))
         except InstanceNotFound:
             # ignore -- if the instance is no more, there is
             # nothing we should free
@@ -1055,7 +1055,7 @@ class OpenStackLrms(LRMS):
         subresource.get_resource_status()
         if len(subresource.job_infos) == 0:
             # turn VM off
-            vm = self._get_vm(app.execution.os_instance_id)
+            vm = self._get_vm(app.execution._lrms_vm_id)
 
             gc3libs.log.info("VM instance %s at %s is no longer needed."
                              " Terminating.", vm.id, vm.preferred_ip)

gc3libs/persistence/store.py
@@ -92,7 +92,24 @@ class Store(object):
         Called immediately after a successful `load()`, just before returning
         the retrieved object to the caller.
         """
-        pass
+        # FIXME: remove after 2.5 release cycle
+        if isinstance(self, gc3libs.Application):
+            if hasattr(self, '_lrms_vm_id'):
+                # already updated, skip
+                pass
+            elif hasattr(self, 'os_instance_id'):
+                self.execution._lrms_vm_id = self.os_instance_id
+                del self.os_instance_id
+            elif hasattr(self, 'ec2_instance_id'):
+                self.execution._lrms_vm_id = self.ec2_instance_id
+                del self.ec2_instance_id
+            elif hasattr(self.execution, 'os_instance_id'):
+                self.execution._lrms_vm_id = self.execution.os_instance_id
+                del self.execution.os_instance_id
+            elif hasattr(self.execution, 'ec2_instance_id'):
+                self.execution._lrms_vm_id = self.execution.ec2_instance_id
+                del self.execution.ec2_instance_id
+
 
     def save(self, obj):
         """