66a122d202b9942aa227c22fc341e00685ace72a
Sergio Maffioletti <sergio.maffioletti@uzh.ch>
2016-09-10 23:34:20 +0200

    Sergio: branch for the training on 12-13 September 2016.

--- docs/programmers/tutorials/workflows/solutions/ex2a.py
+++ docs/programmers/tutorials/workflows/solutions/ex2a.py
@@ -1,14 +1,33 @@
 #! /usr/bin/env python
 
 import os
+from os.path import abspath, basename
 import sys
+import csv
 
+from gc3libs import Application
 from gc3libs.cmdline import SessionBasedScript
 
+def parse(input_csv):
+
+    input_parameters = []
+    
+    with open(input_csv,'rb') as fd:
+        reader = csv.reader(fd)
+        for line in reader:
+            input_parameters.append(line)
+
+    return input_parameters
+
+            
 if __name__ == '__main__':
     from ex2a import AScript
     AScript().run()
 
+# alternatively, you can just copy+paste
+# the code for `GrayscaleApp` here
+from gasset import GassetApplication
+    
 class AScript(SessionBasedScript):
     """
     Minimal workflow scaffolding.
@@ -16,4 +35,8 @@ class AScript(SessionBasedScript):
     def __init__(self):
       super(AScript, self).__init__(version='1.0')
     def new_tasks(self, extra):
-      return []
+        input_params = parse(self.params.args[0])[0]
+        matlab_script = abspath(self.params.args[1])
+        app = GassetApplication(input_params,matlab_script)
+        return [app]
+
