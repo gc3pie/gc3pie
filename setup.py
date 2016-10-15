ae6df38011933009ec40ae0a06de41257098db59
Markus D. Herrmann <HackerMD@users.noreply.github.com>
2016-10-16 00:53:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class Tox(TestCommand):
 #
 setuptools.setup(
     name="gc3pie",
-    version="2.5",  # see PEP 440
+    version="2.5.dev",  # see PEP 440
 
     packages=setuptools.find_packages(exclude=['ez_setup']),
     # metadata for upload to PyPI
