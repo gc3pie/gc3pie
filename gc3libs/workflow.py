44e444f0fc57bb22f24eda1801a1df7b6f921c83
HACKERMD <markusdherrmann@gmail.com>
2016-10-27 03:32:49 +0200

    Handle nested sequential task collections upon `redo()`
    Thanks to @HackerMD for contributing!

--- gc3libs/workflow.py
+++ gc3libs/workflow.py
@@ -394,14 +394,15 @@ class SequentialTaskCollection(TaskCollection):
         """
         Rewind the sequence to a given stage and reset its state to ``NEW``.
         """
-        super(SequentialTaskCollection, self).redo(*args, **kwargs)
-        self._current_task = from_stage
-        task = self.stage()
-        if task is not None:
-            task.redo(*args, **kwargs)
-        # All other tasks should be put in NEW again
-        for i in range(from_stage+1, len(self.tasks)):
-            self.tasks[i].redo(*args, **kwargs)
+        if len(self.tasks) > 0:
+            super(SequentialTaskCollection, self).redo(*args, **kwargs)
+            self._current_task = from_stage
+            task = self.stage()
+            if task is not None:
+                task.redo(*args, **kwargs)
+            # All other tasks should be put in NEW again
+            for i in range(from_stage+1, len(self.tasks)):
+                self.tasks[i].redo(*args, **kwargs)
 
     def submit(self, resubmit=False, targets=None, **extra_args):
         """
