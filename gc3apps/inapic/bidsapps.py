0e6d6acaaca03e5eb3cbf25df70bcce51673ee55
fliem <franz.liem>
2016-12-13 15:20:09 +0100

    initial

--- gc3apps/inapic/bidsapps.py
+++ gc3apps/inapic/bidsapps.py
@@ -36,18 +36,17 @@ Options:
 __version__ = 'development version (SVN $Revision$)'
 # summary of user-visible changes
 __changelog__ = """
-  2015-02-17:
+  2016-12-12:
   * Initial version
 """
-__author__ = 'Sergio Maffioletti <sergio.maffioletti@uzh.ch>'
+__author__ = 'Franz Liem <franziskus.liem@uzh.ch>'
 __docformat__ = 'reStructuredText'
-
 
 # run script, but allow GC3Pie persistence module to access classes defined here;
 # for details, see: https://github.com/uzh/gc3pie/issues/95
 if __name__ == "__main__":
-    import gnift
-    gnift.GniftScript().run()
+    import bidsapps
+    bidsapps.BidsAppsScript().run()
 
 import os
 import sys
@@ -57,6 +56,7 @@ import re
 
 import shutil
 # import csv
+from bids.grabbids import BIDSLayout
 
 from pkg_resources import Requirement, resource_filename
 
@@ -65,48 +65,70 @@ import gc3libs.exceptions
 from gc3libs import Application, Run, Task
 from gc3libs.cmdline import SessionBasedScript, executable_file
 import gc3libs.utils
-from gc3libs.quantity import Memory, kB, MB, MiB, GB, Duration, hours, minutes, seconds
+from gc3libs.quantity import Memory, kB, MB, MiB, GB, Duration, hours, minutes, \
+    seconds
 from gc3libs.workflow import RetryableTask
 
 DEFAULT_CORES = 1
-DEFAULT_MEMORY = Memory(3000,MB)
+DEFAULT_MEMORY = Memory(3000, MB)
 
-DEFAULT_REMOTE_INPUT_FOLDER="./input/"
-DEFAULT_REMOTE_OUTPUT_FOLDER="./output"
+DEFAULT_REMOTE_INPUT_FOLDER = "./input/"
+DEFAULT_REMOTE_OUTPUT_FOLDER = "./output"
+
 
 ## custom application class
-class GniftApplication(Application):
+class BidsAppsApplication(Application):
     """
     """
-    application_name = 'gnifti'
-    
-    def __init__(self, subject, subject_folder, **extra_args):
+    application_name = 'bidsapps'
 
-        self.output_dir = extra_args['output_dir']
+    def __init__(self, subject, bids_input_folder,
+                 #output_folder,
+                 #docker_container, runscript, runscript_args,
+                 **extra_args):
+        self.output_dir = []  # extra_args['output_dir']
 
         inputs = dict()
         outputs = dict()
+        self.output_dir = extra_args['output_dir']
 
-        gnift_wrapper_sh = resource_filename(Requirement.parse("gc3pie"),
-                                              "gc3libs/etc/gnift_wrapper.py")
-        inputs[gnift_wrapper_sh] = os.path.basename(gnift_wrapper_sh)
-        inputs[subject_folder] = DEFAULT_REMOTE_INPUT_FOLDER
+        #fixme
+        # docker_input_folder_str = "{bids_input_folder}:/bids_dataset:ro" \
+        #     .format(bids_input_folder=bids_input_folder)
+        #
+        # docker_output_folder_str = "{output_folder}:/outputs" \
+        #     .format(output_folder=output_folder)
+        #
+        # arguments = "docker run --rm -ti " + \
+        #             "-v {} ".format(docker_input_folder_str) + \
+        #             "-v {} ".format(docker_output_folder_str) + \
+        #             "{docker_container} {runscript} {runscript_args}".format(
+        #                 docker_container=docker_container,
+        #                 runscript=runscript, runscript_args=runscript_args)
 
-        arguments = "./%s %s %s %s" % (inputs[gnift_wrapper_sh],
-                                    subject,
-                                    DEFAULT_REMOTE_INPUT_FOLDER,
-                                    DEFAULT_REMOTE_OUTPUT_FOLDER)
+        arguments = "docker run --rm -ti  {bids_input_folder}".format(
+            bids_input_folder=bids_input_folder)
+
+        # fixme
+        arguments = "echo %s > output/text.txt"%arguments
+        print(arguments)
+        # ""./%s %s %s %s" % (inputs[gnift_wrapper_sh],
+        #                             subject,
+        #                             DEFAULT_REMOTE_INPUT_FOLDER,
+        #                             DEFAULT_REMOTE_OUTPUT_FOLDER)
 
         Application.__init__(
             self,
-            arguments = arguments,
-            inputs = inputs,
-            outputs = [DEFAULT_REMOTE_OUTPUT_FOLDER],
-            stdout = 'gnift.log',
+            arguments=arguments,
+            inputs=[],
+            outputs=[DEFAULT_REMOTE_OUTPUT_FOLDER],
+            stdout='bidsapps.log',
             join=True,
-            **extra_args)        
+            **extra_args)
 
-class GniftScript(SessionBasedScript):
+
+#
+class BidsAppsScript(SessionBasedScript):
     """
     
     The ``gnift`` command keeps a record of jobs (submitted, executed
@@ -118,68 +140,46 @@ class GniftScript(SessionBasedScript):
     Options can specify a maximum number of jobs that should be in
     'SUBMITTED' or 'RUNNING' state; ``gnift`` will delay submission of
     newly-created jobs so that this limit is never exceeded.
+
+    This class is called when bidsapps command is executed.
+    Loops through subjects in bids input folder and starts instance
     """
 
     def __init__(self):
         SessionBasedScript.__init__(
             self,
-            version = __version__, # module version == script version
-            application = GniftApplication, 
-            # only display stats for the top-level policy objects
-            # (which correspond to the processed files) omit counting
-            # actual applications because their number varies over
-            # time as checkpointing and re-submission takes place.
-            stats_only_for = GniftApplication,
-            )
- 
+            version=__version__,  # module version == script version
+            application=BidsAppsApplication,
+            stats_only_for=BidsAppsApplication,
+        )
+
     def setup_args(self):
-
-        self.add_param('input_data', type=str,
+        self.add_param("-i", "--bids_input_folder", type=str,
+                       dest="bids_input_folder", default=None,
                        help="Root localtion of input data. "
-                       "Note: expected folder structure: "
-                       " 1 subfodler for each subject. "
-                       " In each subject folder, " 
-                       " 1 subfolder for each TimePoint. "
-                       " Each TimePoint folder should contain 2 input "
-                       "NFTI files.")
-
+                            "Note: expects folder in BIDS format.")
+       # self.add_param("-x", "--executable", metavar="PATH", #type=executable_file,
+       #                 dest="executable", default=None,
+       #                 help="Path to the GEOtop executable file.")
     def new_tasks(self, extra):
         """
         For each input folder, create an instance of GniftApplication
         """
         tasks = []
 
-        for subject_folder in self.get_input_subject_folder(self.params.input_data):
-        
-            # extract root folder name to be used as jobname
-            subjectname = subject_folder
-
+        for subject_id in self.get_input_subjects(
+                self.params.bids_input_folder):
             extra_args = extra.copy()
-            extra_args['jobname'] = subjectname
+            extra_args['jobname'] = subject_id
 
-            extra_args['output_dir'] = self.params.output
-            extra_args['output_dir'] = extra_args['output_dir'].replace('NAME', 
-                                                                        'run_%s' % subjectname)
-            extra_args['output_dir'] = extra_args['output_dir'].replace('SESSION', 
-                                                                        'run_%s' % subjectname)
-            extra_args['output_dir'] = extra_args['output_dir'].replace('DATE', 
-                                                                        'run_%s' % subjectname)
-            extra_args['output_dir'] = extra_args['output_dir'].replace('TIME', 
-                                                                        'run_%s' % subjectname)
-            
-            tasks.append(GniftApplication(
-                subjectname,
-                os.path.join(self.params.input_data,subject_folder),
+            tasks.append(BidsAppsApplication(
+                subject_id,
                 **extra_args))
-            
+
         return tasks
 
-    def get_input_subject_folder(self, input_folder):
+    def get_input_subjects(self, bids_input_folder):
         """
-        Check and validate input subfolders
-        XXX: for the time being just pass
         """
-        return os.listdir(input_folder)
-        # for folder in os.listdir(input_folder):
-        #     yield os.path.abspath(os.path.join(input_folder,folder))
-            
+        layout = BIDSLayout(bids_input_folder)
+        return layout.get_subjects()
