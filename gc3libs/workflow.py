5eba4dd1e3c3d5096ebb8a0daf76cf0116b1a812
Riccardo Murri <riccardo.murri@gmail.com>
2017-03-22 11:40:16 +0100

    Demote message about `TaskCollection._get_download_dir()` to WARNING level.
    Done in reference to issue #615, to at least mitigate the alarm
    caused by a misplaced error message.

--- gc3libs/workflow.py
+++ gc3libs/workflow.py
@@ -178,7 +178,7 @@ class TaskCollection(Task):
                 return self.output_dir
             except AttributeError:
                 cwd = os.getcwd()
-                gc3libs.log.error(
+                gc3libs.log.warning(
                     "`TaskCollection._get_download_dir()` called"
                     " with no explicit download directory,"
                     " but object '%s' (%s) has no `output_dir`"
