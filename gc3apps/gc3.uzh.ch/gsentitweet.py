6ca5072dca3690380a6a64ccc196f37cd658faef
smaffiol <sergio.maffioletti@uzh.ch>
2017-07-03 07:40:04 +0000

    Sergio and Pim: forst working veersion of gsentitweet.

---
+++ gc3apps/gc3.uzh.ch/gsentitweet.py
@@ -0,0 +1,184 @@
+#! /usr/bin/env python
+#
+#   gsentitweet.py -- Front-end script for evaluating running multiple
+#   Vader sentiment analysis jobs from an initial list of twitter archive
+#   documents.
+#
+#   Copyright (C) 2017, 2018 S3IT, University of Zurich
+#
+#   This program is free software: you can redistribute it and/or
+#   modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Front-end script for running multiple Vader sentiment analysis jobs 
+from an initial list of twitter archive documents.
+
+It uses the generic `gc3libs.cmdline.SessionBasedScript` framework.
+
+See the output of ``gsentitweet.py --help`` for program usage
+instructions.
+"""
+
+# summary of user-visible changes
+__changelog__ = """
+  2017-06-26:
+  * Initial version
+"""
+__author__ = 'Sergio Maffioletti <sergio.maffioletti@uzh.ch>'
+__docformat__ = 'reStructuredText'
+__version__ = '1.0'
+
+# run script, but allow GC3Pie persistence module to access classes defined here;
+# for details, see: http://code.google.com/p/gc3pie/issues/detail?id=95
+if __name__ == "__main__":
+    import gsentitweet
+    gsentitweet.gsentitweetScript().run()
+
+import os
+import sys
+import time
+import tarfile
+import zipfile
+
+from pkg_resources import Requirement, resource_filename
+
+import gc3libs
+import gc3libs.exceptions
+from gc3libs import Application, Run, Task
+from gc3libs.cmdline import SessionBasedScript, executable_file, positive_int, existing_file, existing_directory
+import gc3libs.utils
+from gc3libs.quantity import Memory, kB, MB, GB, Duration, hours, minutes, seconds
+from gc3libs.workflow import RetryableTask
+
+COMMAND = "python twitter-senti-extract.py {twitter_file} {cores}"
+
+## Utility methods
+def _get_twitter_data(input_folder):
+    """
+    Returns list of valid .tar/.zip input files.
+    """
+
+    return [ os.path.join(input_folder,valid_input_files) for valid_input_files in os.listdir(input_folder) if tarfile.is_tarfile(os.path.join(input_folder,
+                                                                                                                    valid_input_files)) or zipfile.is_zipfile(os.path.join(input_folder,
+                                                                                                                                                                          valid_input_files)) ]
+
+## custom application class
+class gsentitweetApplication(Application):
+    """
+    Custom class to wrap the execution of the R scripts passed in src_dir.
+    """
+    application_name = 'gsentitweet'
+    
+    def __init__(self, input_file, getsentiment_script, cores, **extra_args):
+
+        executables = []
+        inputs = dict()
+        outputs = dict()
+
+        if not extra_args['sharedFS']:
+            inputs[input_file] = os.path.basename(input_file)
+            cmd = COMMAND.format(twitter_file=inputs[input_file], cores=cores)
+        else:
+            cmd = COMMAND.format(twitter_file=input_file, cores=cores)
+            
+        if getsentiment_script:
+            inputs[getsentiment_script] = "./twitter-senti-extract.py"
+        else:
+            wrapper = resource_filename(Requirement.parse("gc3pie"),
+                                        "gc3libs/etc/twitter-senti-extract.py")
+            inputs[wrapper] = "./twitter-senti-extract.py"
+
+	extra_args['requested_cores'] = cores
+            
+        Application.__init__(
+            self,
+            arguments = cmd,
+            inputs = inputs,
+            outputs = ["{input_file_name_prefix}.csv".format(input_file_name_prefix=os.path.basename(input_file))],
+            stdout = 'gsentitweet.log',
+            join=True,
+            executables = executables,
+            **extra_args)
+    
+class gsentitweetScript(SessionBasedScript):
+    """
+    The ``gsentitweet`` command keeps a record of jobs (submitted, executed
+    and pending) in a session file (set name with the ``-s`` option); at
+    each invocation of the command, the status of all recorded jobs is
+    updated, output from finished jobs is collected, and a summary table
+    of all known jobs is printed.
+    
+    Options can specify a maximum number of jobs that should be in
+    'SUBMITTED' or 'RUNNING' state; ``gsentitweet`` will delay submission of
+    newly-created jobs so that this limit is never exceeded.
+
+    Once the processing of all chunked files has been completed, ``gsentitweet``
+    aggregates them into a single larger output file located in 
+    'self.params.output'.
+    """
+
+    def __init__(self):
+        SessionBasedScript.__init__(
+            self,
+            version = __version__,
+            application = gsentitweetApplication, 
+            stats_only_for = gsentitweetApplication,
+            )
+
+    def setup_options(self):
+        self.add_param("-P", "--getsentiment", metavar="[PATH]",
+                       type=existing_file,
+                       dest="getsentiment_script", default=None,
+                       help="Location of python script to process input twitter file. "
+                       "Default: %(default)s.")
+        
+        self.add_param("-S", "--sharedfs", dest="shared_FS", 
+                       action="store_true", default=True,
+                       help="Whether the destination resource should assume shared filesystem where Input/Output data will be made available. Data transfer will happen through lcoal filesystem. Default: %(default)s.")
+
+	self.add_param("--cores", dest="core_count", default=4, help="Specifiy number of cores to use Default: %(default)s.")
+
+    def setup_args(self):        
+        self.add_param('input_folder',
+                       type=existing_directory,
+                       help="Path to input folder containing valid input "
+                       " twitter .tar/zip files. Does NOT navigate the folder.")
+        
+    def new_tasks(self, extra):
+        """
+        For each valid input file create a new gsentitweetRetryableTask
+        """
+        tasks = []
+        
+        for input_file in _get_twitter_data(self.params.input_folder):
+            jobname = os.path.basename(input_file)
+                
+            extra_args = extra.copy()
+            extra_args['jobname'] = jobname            
+            extra_args['output_dir'] = self.params.output
+            extra_args['output_dir'] = extra_args['output_dir'].replace('NAME', jobname)
+            extra_args['output_dir'] = extra_args['output_dir'].replace('SESSION', jobname)
+            extra_args['output_dir'] = extra_args['output_dir'].replace('DATE', jobname)
+            extra_args['output_dir'] = extra_args['output_dir'].replace('TIME', jobname)
+            extra_args['sharedFS'] = self.params.shared_FS
+
+            self.log.debug("Creating Application for twitter data '%s'" % (input_file))
+            
+            tasks.append(gsentitweetApplication(
+                input_file,
+                self.params.getsentiment_script,
+		self.params.core_count,
+                **extra_args))
+            
+        return tasks
---
+++ gc3libs/etc/twitter-senti-extract.py
@@ -0,0 +1,195 @@
+import bz2
+import json
+import os
+import re
+import sys
+import time
+import tarfile
+from multiprocessing import Manager, Pool, Process, current_process
+from zipfile import ZipFile, is_zipfile
+
+import pandas
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+
+
+def parse_magic_ass_json(data):
+    """
+    The JSON format of twitter JSON files is JSONL-ish, parse it to lines of tweets (kv pairs)
+    :param data: twitter json file
+    :return: list of tweets (as kv pairs)
+    """
+    result = []
+    print('parsing json (size: {0})'.format(len(data)))
+    try:
+        data = bz2.decompress(data)
+        print('decompressed data to {0}'.format(len(data)))
+    except IOError:
+        pass
+    for line in data.split('\n'):
+        try:
+            result.append(json.loads(line))
+        except ValueError:
+            pass
+    return result
+
+
+def extract_tweets(lines):
+    """
+    Extract the sentiment from a bunch of tweets
+    :param lines: buch of lines containing kv pairs of twitter data
+    :return: mean of negative, positive, neutral and compound sentiment in the lines
+    """
+    skipped = 0L
+    result = []
+    for tweet in lines:
+        if 'text' in tweet:
+            scores = analyzer.polarity_scores(tweet.get('text'))
+        elif 'body' in tweet:
+            scores = analyzer.polarity_scores(tweet.get('body'))
+        else:
+            skipped += 1
+            continue
+        result.append([scores.get('neg'), scores.get('pos'), scores.get('neu'), scores.get('compound')])
+    print('{0} tweets with no text/body field ({1} total), they have been skipped'.format(skipped, len(lines)))
+    df = pandas.DataFrame(result, columns=['negative', 'positive', 'neutral', 'compound'])
+    return df['negative'].mean(), df['positive'].mean(), df['neutral'].mean(), df['compound'].mean()
+
+
+def process_twitter_json_file(archive_path, y, m, fq, wq):
+    """
+    Worker that processes a single twitter json file from the archive   
+    :param archive_path: name of the archive
+    :param y: cached year value
+    :param m: cached month value
+    :param fq: queue to read from
+    :param wq: queue to write to
+    """
+
+    print('Starting process_twitter_json_file named {0}'.format(current_process().name))
+
+    while True:
+        archive_file = fq.get()
+        if isinstance(archive_file, basestring) and archive_file == 'EOP':
+            break
+        else:
+            print('going to extract archive file {0} from {1}'.format(archive_file.name, archive_path))
+            with tarfile.open(archive_path) as tf:
+                extracted_archive_file = tf.extractfile(archive_file)
+                if not extracted_archive_file.closed:
+                    json_data = extracted_archive_file.read()
+                    data = parse_magic_ass_json(json_data)
+                    extracted_archive_file.close()
+                    neg, pos, neu, com = extract_tweets(data)
+                    if len(archive_file.name.split('/')) == 5:
+                        yr, mt, day, hour, p = archive_file.name.split('/')
+                    elif len(archive_file.name.split('/')) == 4 and year:
+                        yr = y
+                        mt, day, hour, p = archive_file.name.split('/')
+                    elif len(archive_file.name.split('/')) == 3 and year and month:
+                        yr = y
+                        mt = m
+                        day, hour, p = archive_file.name.split('/')
+                    else:
+                        raise ValueError('cannot infer date from filename and/or directory structure')
+                    wq.put('{y}.{m}.{d}.{h}.{p};{neg};{pos};{neu};{com};\n'.format(y=yr,
+                                                                                   m=mt,
+                                                                                   d=day,
+                                                                                   p=p.split('.')[0],
+                                                                                   h=hour,
+                                                                                   neg=neg,
+                                                                                   pos=pos,
+                                                                                   neu=neu,
+                                                                                   com=com))
+                else:
+                    print('file pointer not opened, something went wrong for {0} in {1}, skipping..,'.format(archive_file.name, archive_path))
+
+
+def mp_writer(o, wq):
+    """
+    Multiprocess listener. Listens to message on Queue and writes them to file
+    :param o: name of the output file
+    :param wq: queue to read from 
+    """
+    with open(o, 'wb') as fp:
+        while True:
+            m = wq.get()
+            if m == 'EOP':
+                fp.flush()
+                break
+            fp.write(m)
+            fp.flush()
+
+
+def queue_archive_files(archive_path, fq):
+    """
+    Extract all data and process everything :)
+    :param archive_path: name of the archive
+    :param fq: puts the file names on the queue 
+    """
+    if is_zipfile(archive_path):
+        with ZipFile(archive_path, 'r') as archive:
+            for archive_file in archive.namelist():
+                if not archive_file.endswith('/') and ZipFile.getinfo(archive, archive_file).file_size > 0:
+                    fq.put(archive_file)
+    elif tarfile.is_tarfile(archive_path):
+        with tarfile.open(archive_path) as archive:
+            for archive_file in archive:
+                if archive_file.isreg() and archive_file.size > 0:
+                    fq.put(archive_file)
+
+
+if __name__ == "__main__":
+    # wait {timeout} seconds for our file to become available
+    timeout = 360
+    if len(sys.argv) == 3:
+        while not os.path.exists(sys.argv[1]):
+            timeout -= 1
+            if timeout > 0:
+                print('file {0} not available yet, waiting {1} more seconds'.format(sys.argv[1], timeout))
+            else:
+                print('timout exceeded, terminating program, please check configuration for issues')
+                exit(-2)
+            time.sleep(1)
+        year = None
+        month = None
+        match = re.search(r'([0-9]{4})-([0-9]{2})', sys.argv[1])
+        if len(match.groups()) == 2:
+            year = match.group(1)
+            month = match.group(2)
+            print('found something resembling a year/month thingy ({0}-{1})in the file name, caching it.'.format(year, month))
+        print('setting up multiprocessing')
+        manager = Manager()
+        file_queue = manager.Queue()
+        writer_queue = manager.Queue()
+        print('Setup our file indexer')
+        indexer = Process(target=queue_archive_files, args=(sys.argv[1], file_queue,))
+        indexer.start()
+        print('Setup our output writer')
+        writer = Process(target=mp_writer, args=('{fn}.csv'.format(fn=os.path.basename(sys.argv[1])), writer_queue,))
+        writer.start()
+        print('Setup file processors and start crunching')
+	jobs = []
+	for index in range(0,int(sys.argv[2])):
+	    jobs.append(Process(target=process_twitter_json_file, args=(sys.argv[1], year, month, file_queue, writer_queue,)))
+   	for job in jobs:
+	    job.start()
+	    print('Started process {0} in pid {1}'.format(job.name,job.pid))
+	print('wait for our indexer to finish')
+        indexer.join()
+        print('add terminators to index file queue (as last element)')
+        for job in jobs:
+            file_queue.put('EOP')
+        print('wait for our processors to finnish')
+        for job in jobs:
+            job.join()
+        print('and now add terminator for writer and wait to finish')
+        writer_queue.put('EOP')
+        writer.join()
+        print('Elvis has left the building')
+    else:
+        print('Invalid argument count, should only be one single archive and a cpu core count.')
+        exit(-1)
+
+
