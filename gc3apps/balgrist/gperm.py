6b37b7b60e25592ffb2e210a8f5d1e8639b35bae
Sergio Maffioletti <sergio.maffioletti@uzh.ch>
2017-07-07 14:13:40 +0200

    Sergio: working version of gperm: utility for running BIDS/apps on Docker containers.

---
+++ gc3apps/balgrist/gperm.py
@@ -0,0 +1,222 @@
+#! /usr/bin/env python
+#
+#   gperm.py -- Front-end script for running Docker BIDS apps
+#   function over a large dataset.
+#
+#   Copyright (C) 2017, 2018 S3IT, University of Zurich
+#
+#   This program is free software: you can redistribute it and/or
+#   modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Front-end script for submitting multiple `BIDS apps` jobs fetching 
+docker images from the `BIDS apps` repository.
+
+It uses the generic `gc3libs.cmdline.SessionBasedScript` framework.
+
+See the output of ``gperm.py --help`` for program usage
+instructions.
+
+Example of docker execution:
+docker run -i --rm -v /mnt/filo/data/ds005:/bids_dataset:ro -v /mnt/filo/outputs:/outputs bids/fmriprep /bids_dataset /outputs participant --participant_label 01
+
+gperm takes BIDS files as input.
+"""
+
+# summary of user-visible changes
+__changelog__ = """
+  2017-04-18:
+  * Initial version
+"""
+__author__ = 'Sergio Maffioletti <sergio.maffioletti@uzh.ch>'
+__docformat__ = 'reStructuredText'
+__version__ = '1.0'
+
+# run script, but allow GC3Pie persistence module to access classes defined here;
+# for details, see: http://code.google.com/p/gc3pie/issues/detail?id=95
+if __name__ == "__main__":
+    import gperm
+    gperm.GpermScript().run()
+
+import os
+import sys
+import time
+import tempfile
+import mimetypes
+import random
+
+from pkg_resources import Requirement, resource_filename
+
+import gc3libs
+import gc3libs.exceptions
+from gc3libs import Application, Run, Task
+from gc3libs.cmdline import SessionBasedScript, executable_file, positive_int, existing_file, existing_directory
+import gc3libs.utils
+from gc3libs.quantity import Memory, kB, MB, GB, Duration, hours, minutes, seconds
+from gc3libs.workflow import RetryableTask
+
+DEFAULT_BIDS_FOLDER="data/"
+DEFAULT_RESULT_FOLDER="output/"
+DEFAULT_DOCKER_BIDS_APP="poldracklab/fmriprep"
+DEFAULT_DOCKER_BIDS_ARGS=""
+DEFAULT_REPETITIONS=1
+
+## Utility methods
+def _get_subjects(input_folder):
+    """
+    return tuple ([list .json and .tsv files],[list of sub-folders])
+    Assumptions:
+    * each sub-folder contains a valid subject's data
+    * each .json and .tsv file found in root folder will be made available
+    to all Applications.
+    """
+    control_files_list = []
+    subjects_folders_list = []
+    
+    for element in os.listdir(input_folder):
+        full_element = os.path.join(input_folder,element)
+        if os.path.isfile(full_element) and (element.endswith(".json") or
+                                             element.endswith(".tsv")):
+            # Valid control file
+            control_files_list.append(full_element)
+        elif os.path.isdir(full_element):
+            # Valid subject folder
+            subjects_folders_list.append(full_element)
+
+    return (control_files_list,subjects_folders_list)
+
+## custom application class
+class GpermApplication(Application):
+    """
+    Custom class to wrap the execution of the R scripts passed in src_dir.
+    """
+    application_name = 'gperm'
+    
+    def __init__(self, subject, subject_name, control_files, repeat_index, docker_image, docker_args, **extra_args):
+
+        executables = []
+        inputs = dict()
+        outputs = dict()
+        
+        inputs[subject] = os.path.join(DEFAULT_BIDS_FOLDER,
+                                       os.path.basename(subject))
+
+        # add all control files to 'data' folder
+        for element in control_files:
+            inputs[element] = os.path.join(DEFAULT_BIDS_FOLDER,
+                                       os.path.basename(element))
+
+
+        wrapper = resource_filename(Requirement.parse("gc3pie"),
+                                    "gc3libs/etc/gperm_wrapper.sh")
+
+        inputs[wrapper] = "./wrapper.sh"
+        executables.append(inputs[wrapper])
+
+        arguments = "./wrapper.sh {subject} {subject_name} {output} {docker_app} {docker_args}".format(subject=DEFAULT_BIDS_FOLDER,
+                                                                                                       subject_name=subject_name,
+                                                                                                       output=DEFAULT_RESULT_FOLDER,
+                                                                                                       docker_app=docker_image,
+                                                                                                       docker_args=docler_args)
+        gc3libs.log.debug("Creating application for executing: %s", arguments)
+        
+        Application.__init__(
+            self,
+            arguments = arguments,
+            inputs = inputs,
+            outputs = [DEFAULT_RESULT_FOLDER],
+            stdout = 'gperm.log',
+            join=True,
+            executables = executables,
+            **extra_args)    
+    
+class GpermScript(SessionBasedScript):
+    """
+    The ``gperm`` command keeps a record of jobs (submitted, executed
+    and pending) in a session file (set name with the ``-s`` option); at
+    each invocation of the command, the status of all recorded jobs is
+    updated, output from finished jobs is collected, and a summary table
+    of all known jobs is printed.
+    
+    Options can specify a maximum number of jobs that should be in
+    'SUBMITTED' or 'RUNNING' state; ``gperm`` will delay submission of
+    newly-created jobs so that this limit is never exceeded.
+
+    Once the processing of all chunked files has been completed, ``gperm``
+    aggregates them into a single larger output file located in 
+    'self.params.output'.
+    """
+
+    def __init__(self):
+        SessionBasedScript.__init__(
+            self,
+            version = __version__,
+            application = GpermApplication, 
+            stats_only_for = GpermApplication,
+            )
+
+    def setup_args(self):        
+        self.add_param('input_folder',
+                       type=existing_directory,
+                       help="Path to input folder containing valid input .bids files.")
+
+    def setup_options(self):
+        self.add_param("-R", "--repeat", metavar="[INT]",
+                       type=positive_int,
+                       dest="repeat", default=DEFAULT_REPETITIONS,
+                       help="Repeat analysis. Default: %(default)s.")
+
+        self.add_param("-D", "--docker", metavar="[PATH]",
+                       dest="docker_image", default=DEFAULT_DOCKER_BIDS_APP,
+                       help="BIDS app docker image. Default: %(default)s.")
+
+        self.add_param("-D_args", "--use-aroma", metavar="[STRING]",
+                       dest="docker_args", default=DEFAULT_DOCKER_BIDS_ARGS,
+                       help="BIDS app docker arguments. Default: %(default)s.")
+        
+    def new_tasks(self, extra):
+        """
+        For each valid input file create a new GpermRetryableTask
+        """
+        tasks = []
+        
+        control_files,subjects_list = _get_subjects(self.params.input_folder)
+        for subject in subjects_list:
+            for repeat in range(0,self.params.repeat):
+                subject_name = os.path.basename(subject)
+                jobname = "{subject}-{rep}".format(subject=subject_name,
+                                                   rep=repeat)
+                
+                extra_args = extra.copy()
+
+                extra_args['jobname'] = jobname            
+                extra_args['output_dir'] = self.params.output
+                extra_args['output_dir'] = extra_args['output_dir'].replace('NAME', jobname)
+                extra_args['output_dir'] = extra_args['output_dir'].replace('SESSION', jobname)
+                extra_args['output_dir'] = extra_args['output_dir'].replace('DATE', jobname)
+                extra_args['output_dir'] = extra_args['output_dir'].replace('TIME', jobname)
+
+                self.log.debug("Creating Application for subject %s repetition : %d" % (subject_name,
+                                                                                        repeat))
+
+                tasks.append(GpermApplication(
+                    subject,
+                    subject_name,
+                    control_files,
+                    repeat,
+                    self.params.docker_image,
+                    self.params.docker_args,                    
+                    **extra_args))
+
+        return tasks
---
+++ gc3libs/etc/gperm_wrapper.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#
+# gperm_wrapper.sh -- base wrapper script for executing Docker BIDS apps
+# 
+# Authors: Sergio Maffioletti <sergio.maffioletti@uzh.ch>
+#
+#   Copyright (C) 2017, 2018 S3IT, University of Zurich
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+#-----------------------------------------------------------------------
+
+PROG=$(basename "$0")
+DEBUG=1
+
+## helper functions
+function log {
+    if [ $DEBUG -ne 0 ]; then
+	echo "DEBUG: $1"
+    fi
+}
+
+function die () {
+  rc="$1"
+  shift
+  (echo -n "$PROG: ERROR: ";
+      if [ $# -gt 0 ]; then echo "$@"; else cat; fi) 1>&2
+  exit $rc
+}
+
+## Parse command line options.
+USAGE=`cat <<EOF
+Usage: 
+  $PROG <subject folder> <subject name> <result folder> <docker image>
+EOF`
+
+# We want 4 non-option argument:
+if [ ! $# -eq 4 ]; then
+    die 1 "$USAGE"
+fi
+
+# Check input arguments
+SUBJECT_DIR=$PWD/$1
+SUBJECT_NAME=$2
+OUTPUT_DIR=$PWD/$3
+DOCKER_TO_RUN=$4
+DOCKER_ARGS=$5
+
+# Create output folder if not present
+if ![ -d $OUTPUT_DIR ]; then
+    mkdir -p OUTPUT_DIR
+fi
+
+echo "[`date +%Y-%m-%d" "%H:%M:%S`] Start"
+
+log "Subject dir: ${SUBJECT_DIR}"
+log "Subject name: ${SUBJECT_NAME}"
+log "Output dir: ${OUTPUT_DIR}"
+log "Using Docker image: ${DOCKER_TO_RUN}"
+log "Using Docker arguments: ${DOCKER_ARGS}"
+
+# run script
+echo "docker run -i --rm -v ${SUBJECT_DIR}:/bids:ro -v ${OUTPUT_DIR}:/output ${DOCKER_TO_RUN} /bids /output participant --participant_label ${SUBJECT_NAME} --no-submm-recon ${DOCKER_ARGS}"
+docker run -i --rm -v ${SUBJECT_DIR}:/bids:ro -v ${OUTPUT_DIR}:/output ${DOCKER_TO_RUN} /bids /output participant --participant_label ${SUBJECT_NAME} --no-submm-recon ${DOCKER_ARGS}
+RET=$?
+
+log "Simulation ended with exit code $RET"
+
+# Prepare result
+echo "[`date +%Y-%m-%d" "%H:%M:%S`] Done"
+
+exit $RET
+
