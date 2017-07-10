f83d14ae1577e58bea7c1e12de9ce47a3ae91045
Sergio Maffioletti <sergio.maffioletti@uzh.ch>
2017-07-10 15:03:46 +0200

    Sergio: set jobname to each step of the pipeline

--- gc3apps/ipz.uzh/gregress.py
+++ gc3apps/ipz.uzh/gregress.py
@@ -1,9 +1,8 @@
 #! /usr/bin/env python
 #
-#   genREM.py -- Front-end script for running ParRecoveryFun Matlab 
-#   function with a given combination of reference models.
+#   gregress.py -- Front-end script for running REMDataset pipeline.
 #
-#   Copyright (c) 2015 S3IT, University of Zurich, http://www.s3it.uzh.ch/
+#   Copyright (c) 2017, 2018 S3IT, University of Zurich, http://www.s3it.uzh.ch/
 #
 #   This program is free software: you can redistribute it and/or
 #   modify
@@ -26,7 +25,7 @@ It uses the generic `gc3libs.cmdline.SessionBasedScript` framework.
 
 # summary of user-visible changes
 __changelog__ = """
-  2015-11-17:
+  2017-07-03:
   * Initial version
 """
 __author__ = 'Sergio Maffioletti <sergio.maffioletti@uzh.ch>'
@@ -41,14 +40,8 @@ if __name__ == "__main__":
 
 import os
 import sys
-import time
-import tempfile
-import re
-
 import shutil
 import random
-import posix
-
 from pkg_resources import Requirement, resource_filename
 
 import gc3libs
@@ -59,10 +52,11 @@ from gc3libs.cmdline import SessionBasedScript, executable_file, \
 import gc3libs.utils
 from gc3libs.quantity import Memory, kB, MB, MiB, GB, Duration, \
     hours, minutes, seconds
-from gc3libs.workflow import RetryableTask, StagedTaskCollection, \
+from gc3libs.workflow import StagedTaskCollection, \
     ParallelTaskCollection, SequentialTaskCollection
 
-# Constanst
+# Defaults
+
 S0_REMOTE_INPUT_FILENAME = "./input.stata"
 S0_REMOTE_OUTPUT_FILENAME = "./output.RData"
 RSCRIPT_COMMAND="Rscript --vanilla {src}/script.R {method} {src} {data}"
@@ -75,13 +69,15 @@ REMOTE_RESULT_FOLDER="result"
 REMOTE_DATA_FOLDER="data"
 REMOTE_SCRIPTS_FOLDER="src"
 
+
+
+
 # Utility methods
 
-## Custom application class
 class GenREMDatasetApplication(Application):
     """
-    Transform input .stata file into rem DataFrame
-    Could run locally provided R is installed.
+    Execute main workflow script and pass the workflow step
+    as input argument.
     """
     application_name = 'genREM'
     
@@ -91,10 +87,11 @@ class GenREMDatasetApplication(Application):
 
         self.output = extra_args['results']
 
-        for data_file in os.listdir(data_file_list):
-            inputs[os.path.join(data_file_list,
-                                data_file)] = os.path.join(REMOTE_DATA_FOLDER,
-                                                           data_file)
+        for data_folder in data_file_list:
+            for data_file in os.listdir(data_folder):
+                inputs[os.path.join(data_folder,
+                                    data_file)] = os.path.join(REMOTE_DATA_FOLDER,
+                                                               data_file)
 
         for script_file in os.listdir(source_folder):
             inputs[os.path.join(source_folder,
@@ -116,7 +113,7 @@ class GenREMDatasetApplication(Application):
 
     def terminated(self):
         """
-        Move results to 'result' folder
+        Move results to corresponding 'result' folder
         """
 
         if not os.path.isdir(self.output):
@@ -131,8 +128,10 @@ class GenREMDatasetApplication(Application):
         
 class GenREMStagedTaskCollection(StagedTaskCollection):
     """
-    Stage0: Take input .stata file and convert it into Rem
-    Dataframe.
+    Staged collection:
+    Step 0: Generate REMDataset
+    Step 1: For each available statistical method, run independent application
+    Step 2: Merge all results together
     """
     def __init__(self, data_folder, source_folder, **extra_args):
 
@@ -141,13 +140,12 @@ class GenREMStagedTaskCollection(StagedTaskCollection):
         self.extra = extra_args
         self.s0_outputfolder = os.path.join(extra_args['result'],"S0")
         self.s1_outputfolder = os.path.join(extra_args['result'],"S1")
-        self.s2_outputfolder = os.path.join(extra_args['result'],"S2")
+        self.s2_outputfolder = os.path.join(extra_args['result'],"S2")        
         StagedTaskCollection.__init__(self)
             
     def stage0(self):
         """
-        Transform input .stata into rem DataFrame by calling
-        ENB_RemDatasetApplication.
+        Step 0: Generate REMDataset
         """
 
         extra_args = self.extra.copy()
@@ -164,40 +162,57 @@ class GenREMStagedTaskCollection(StagedTaskCollection):
         # gc3libs.log.debug("Creating Stage0 task for : %s" % os.path.basename(self.input_stata_file))
         extra_args['results'] = self.s0_outputfolder
 
-        return GenREMDatasetApplication("remdataset",self.data_folder,self.source_folder,**extra_args)
+        return GenREMDatasetApplication("remdataset",[self.data_folder],self.source_folder,**extra_args)
 
 
     def stage1(self):
         """
+        Step 1: For each available statistical method, run independent application
         """
-        # XXX: add check if stage0 completed properly
-        # Stop otherwise
         tasks = []
 
         for method in STATS:
             extra_args = self.extra.copy()
             extra_args['jobname'] = method
             extra_args['results'] = self.s1_outputfolder
-            tasks.append(GenREMDatasetApplication(method,self.s0_outputfolder,self.source_folder,**extra_args))
+            extra_args['output_dir'] = extra_args['output_dir'].replace('NAME', 
+                                                                        extra_args['jobname'])
+            extra_args['output_dir'] = extra_args['output_dir'].replace('SESSION', 
+                                                                        extra_args['jobname'])
+            extra_args['output_dir'] = extra_args['output_dir'].replace('DATE', 
+                                                                        extra_args['jobname'])
+            extra_args['output_dir'] = extra_args['output_dir'].replace('TIME', 
+                                                                        extra_args['jobname'])
+
+            tasks.append(GenREMDatasetApplication(method,[self.s0_outputfolder],self.source_folder,**extra_args))
         return ParallelTaskCollection(tasks)
 
     def stage2(self):
         """
+        Step 2: Merge all results together
         """
         extra_args = self.extra.copy()
         extra_args['jobname'] = "merge"
         extra_args['results'] = self.s2_outputfolder
-        return GenREMDatasetApplication("merge",self.data_folder,self.source_folder,**extra_args)
+        extra_args['output_dir'] = extra_args['output_dir'].replace('NAME', 
+                                                                    extra_args['jobname'])
+        extra_args['output_dir'] = extra_args['output_dir'].replace('SESSION', 
+                                                                    extra_args['jobname'])
+        extra_args['output_dir'] = extra_args['output_dir'].replace('DATE', 
+                                                                    extra_args['jobname'])
+        extra_args['output_dir'] = extra_args['output_dir'].replace('TIME', 
+                                                                    extra_args['jobname'])
+
+        return GenREMDatasetApplication("merge",[self.s0_outputfolder, self.s1_outputfolder],self.source_folder,**extra_args)
 
         
 class GenREMScript(SessionBasedScript):
     """
-    For each param file (with '.mat' extension) found in the 'param folder',
-    GscrScript extracts the corresponding index (from filename) and searches for
-    the associated file in 'data folder'. For each pair ('param_file','data_file'),
-    GscrScript generates execution Tasks.
+    Take initial RData DataFrame input file and run the full workflow by
+    passing to the main execution script the workflow steps as part of the input
+    arguments.
     
-    The ``gscr`` command keeps a record of jobs (submitted, executed
+    The ``gregress`` command keeps a record of jobs (submitted, executed
     and pending) in a session file (set name with the ``-s`` option); at
     each invocation of the command, the status of all recorded jobs is
     updated, output from finished jobs is collected, and a summary table
@@ -211,9 +226,9 @@ class GenREMScript(SessionBasedScript):
     def __init__(self):
         SessionBasedScript.__init__(
             self,
-            version = __version__, # module version == script version
-            application = Application,
-            stats_only_for = Application
+            version = __version__,
+            application = GenREMDatasetApplication,
+            stats_only_for = GenREMDatasetApplication
             )
 
     def setup_args(self):
@@ -222,8 +237,6 @@ class GenREMScript(SessionBasedScript):
                        help="Location of initial RData file.")
 
     def setup_options(self):
-        """
-        """
         self.add_param("-x", "--source", metavar="PATH", 
                        dest="src", default=None,
                        type=existing_directory,
@@ -235,13 +248,10 @@ class GenREMScript(SessionBasedScript):
 
     def new_tasks(self, extra):
         """
-        For each of the network data and for each of the selected benchmarks,
-        create a GscrApplication.
-
-        First loop the input files, then loop the selected benchmarks
+        Just start the StageCollection with the passed input RData file in
+        self.params.data_folder.
         """
         extra_args = extra.copy()
-        #extra_args.update(self.params.__dict__)
         extra_args['result'] = self.params.result
         return [GenREMStagedTaskCollection(self.params.data_folder,
                                            self.params.src,
