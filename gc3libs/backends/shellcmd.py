7c127e1b6d4928fd27d9d69885e9841d5496696a
Sergio Maffioletti <sergio.maffioletti@uzh.ch>
2017-08-11 15:17:53 +0200

    Sergio: avoid deleting ShellCmd remote job's PID files as part of the '_cleanup_terminating_task' as the method get called in several differnt phases of the job lifecycle. Removal of remote job's PID file should only be part of the 'free' method.

--- gc3libs/backends/shellcmd.py
+++ gc3libs/backends/shellcmd.py
@@ -1058,7 +1058,7 @@ class ShellcmdLrms(LRMS):
             else:
                 # ignore
                 pass
-        self._delete_job_info_file(root_pid)
+        # self._delete_job_info_file(root_pid)
 
     def _grace_time(self, wait):
         if wait:
@@ -1610,8 +1610,8 @@ class ShellcmdLrms(LRMS):
                    .format(wrapper_filename, app, err))
             log.warning("%s -- Termination status and resource utilization fields will not be set.", msg)
             raise gc3libs.exceptions.InvalidValue(msg)
-        finally:
-            self._delete_job_info_file(pid)
+        # finally:
+        #     self._delete_job_info_file(pid)
 
     def _parse_wrapper_output(self, wrapper_file):
         """
