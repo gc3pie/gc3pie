8f7b00345b85f6d03088ee594ac4fe8014515bf5
Riccardo Murri <riccardo.murri@gmail.com>
2019-08-01 13:01:55 +0200

    gc3libs.utils: Remove unused function `send_mail`.

--- gc3libs/utils.py
+++ gc3libs/utils.py
@@ -1865,36 +1865,6 @@ def to_bytes(s):
     return int(num) * (k ** BYTE_SUFFIX_TO_POW[unit])
 
 
-def send_mail(send_from, send_to, subject, text, files=[], server="localhost"):
-
-    from smtplib import SMTP
-    from email.MIMEMultipart import MIMEMultipart
-    from email.MIMEBase import MIMEBase
-    from email.MIMEText import MIMEText
-    from email.Utils import COMMASPACE, formatdate
-    from email import Encoders
-
-    msg = MIMEMultipart()
-    msg['From'] = send_from
-    msg['To'] = COMMASPACE.join(send_to)
-    msg['Date'] = formatdate(localtime=True)
-    msg['Subject'] = subject
-
-    msg.attach(MIMEText(text))
-
-    for f in files:
-        part = MIMEBase('application', "octet-stream")
-        part.set_payload(open(f, "rb").read())
-        Encoders.encode_base64(part)
-        part.add_header('Content-Disposition',
-                        'attachment; filename="%s"' % os.path.basename(f))
-        msg.attach(part)
-
-    smtp = SMTP(server)
-    smtp.sendmail(send_from, send_to, msg.as_string())
-    smtp.close()
-
-
 def touch(path):
     """
     Ensure a regular file exists at `path`.
