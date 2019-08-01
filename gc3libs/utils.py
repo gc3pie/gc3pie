d09f7f49323cd1fc8194fc0f1142e0316d94d2bf
Riccardo Murri <riccardo.murri@gmail.com>
2019-08-01 12:59:37 +0200

    gc3libs/utils.py: Move function `check_file_access` to its place.
    Functions in `gc3libs/utils.py` are sorted according to alphabetical
    order.

--- gc3libs/utils.py
+++ gc3libs/utils.py
@@ -261,6 +261,66 @@ def cat(*args, **extra_args):
             output.write(line)
 
 
+def check_file_access(path, mode, exception=RuntimeError, isdir=False):
+    """
+    Test for access to a path; if access is not granted, raise an
+    instance of `exception` with an appropriate error message.
+    This is a frontend to `os.access`:func:, which see for exact
+    semantics and the meaning of `path` and `mode`.
+
+    :param path: Filesystem path to test.
+    :param mode: See `os.access`:func:
+    :param exception: Class of exception to raise if test fails.
+    :param isdir: If `True` then also test that `path` points to a directory.
+
+    If the test succeeds, `True` is returned::
+
+      >>> check_file_access('/bin/cat', os.F_OK)
+      True
+      >>> check_file_access('/bin/cat', os.R_OK)
+      True
+      >>> check_file_access('/bin/cat', os.X_OK)
+      True
+      >>> check_file_access('/tmp', os.X_OK)
+      True
+
+    However, if the test fails, then an exception is raised::
+
+      >>> check_file_access('/bin/cat', os.W_OK)
+      Traceback (most recent call last):
+        ...
+      RuntimeError: Cannot write to file '/bin/cat'.
+
+    If the optional argument `isdir` is `True`, then additionally test
+    that `path` points to a directory inode::
+
+      >>> check_file_access('/tmp', os.F_OK, isdir=True)
+      True
+
+      >>> check_file_access('/bin/cat', os.F_OK, isdir=True)
+      Traceback (most recent call last):
+        ...
+      RuntimeError: Expected '/bin/cat' to be a directory, but it's not.
+    """
+    what = ("directory" if isdir else "file")
+    if not os.access(path, os.F_OK):
+        raise exception("Cannot access %s '%s'." % (what, path))
+    if isdir and not os.path.isdir(path):
+        raise exception(
+            "Expected '%s' to be a directory, but it's not." % path)
+    if (mode & os.R_OK) and not os.access(path, os.R_OK):
+        raise exception("Cannot read %s '%s'." % (what, path))
+    if (mode & os.W_OK) and not os.access(path, os.W_OK):
+        raise exception("Cannot write to %s '%s'." % (what, path))
+    if (mode & os.X_OK) and not os.access(path, os.X_OK):
+        if isdir:
+            raise exception("Cannot traverse directory '%s':"
+                            " lacks 'x' permission." % path)
+        else:
+            raise exception("File '%s' lacks execute ('x') permission." % path)
+    return True
+
+
 def deploy_configuration_file(filename, template_filename=None):
     """
     Ensure that configuration file `filename` exists; possibly
@@ -1766,66 +1826,6 @@ def tempdir(**kwargs):
     finally:
         if os.path.isdir(tmpdir):
             shutil.rmtree(tmpdir, ignore_errors=True)
-
-
-def check_file_access(path, mode, exception=RuntimeError, isdir=False):
-    """
-    Test for access to a path; if access is not granted, raise an
-    instance of `exception` with an appropriate error message.
-    This is a frontend to `os.access`:func:, which see for exact
-    semantics and the meaning of `path` and `mode`.
-
-    :param path: Filesystem path to test.
-    :param mode: See `os.access`:func:
-    :param exception: Class of exception to raise if test fails.
-    :param isdir: If `True` then also test that `path` points to a directory.
-
-    If the test succeeds, `True` is returned::
-
-      >>> check_file_access('/bin/cat', os.F_OK)
-      True
-      >>> check_file_access('/bin/cat', os.R_OK)
-      True
-      >>> check_file_access('/bin/cat', os.X_OK)
-      True
-      >>> check_file_access('/tmp', os.X_OK)
-      True
-
-    However, if the test fails, then an exception is raised::
-
-      >>> check_file_access('/bin/cat', os.W_OK)
-      Traceback (most recent call last):
-        ...
-      RuntimeError: Cannot write to file '/bin/cat'.
-
-    If the optional argument `isdir` is `True`, then additionally test
-    that `path` points to a directory inode::
-
-      >>> check_file_access('/tmp', os.F_OK, isdir=True)
-      True
-
-      >>> check_file_access('/bin/cat', os.F_OK, isdir=True)
-      Traceback (most recent call last):
-        ...
-      RuntimeError: Expected '/bin/cat' to be a directory, but it's not.
-    """
-    what = ("directory" if isdir else "file")
-    if not os.access(path, os.F_OK):
-        raise exception("Cannot access %s '%s'." % (what, path))
-    if isdir and not os.path.isdir(path):
-        raise exception(
-            "Expected '%s' to be a directory, but it's not." % path)
-    if (mode & os.R_OK) and not os.access(path, os.R_OK):
-        raise exception("Cannot read %s '%s'." % (what, path))
-    if (mode & os.W_OK) and not os.access(path, os.W_OK):
-        raise exception("Cannot write to %s '%s'." % (what, path))
-    if (mode & os.X_OK) and not os.access(path, os.X_OK):
-        if isdir:
-            raise exception("Cannot traverse directory '%s':"
-                            " lacks 'x' permission." % path)
-        else:
-            raise exception("File '%s' lacks execute ('x') permission." % path)
-    return True
 
 
 def to_bytes(s):
