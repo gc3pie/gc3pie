e1d6c9355898da2d25070f982168ea77bd141ef2
Riccardo Murri <riccardo.murri@gmail.com>
2021-08-06 17:54:26 +0200

    Release version 2.6.6

--- gc3libs/__init__.py
+++ gc3libs/__init__.py
@@ -38,7 +38,7 @@ from past.utils import old_div
 from builtins import object
 __docformat__ = 'reStructuredText'
 
-__version__ = '2.6.5'
+__version__ = '2.6.6'
 
 
 import os
@@ -86,7 +86,7 @@ it stands for 'fetch the whole contents of the remote
 directory'.
 """
 
-# for compatibility with GC3Pie <2.6.5
+# for compatibility with GC3Pie <2.6.6
 Default = gc3libs.defaults
 
 
